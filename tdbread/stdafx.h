a269da3aaec4ea0a8773475dd0dc3baed468b260
Richard Otis <rao140@psu.edu>
2013-03-09 15:32:36 -0500

    Made the libTDB parsers less chatty.

--- tdbread/stdafx.h
+++ tdbread/stdafx.h
@@ -1,6 +1,5 @@
 // stdafx.h -- list of precompiled headers
 
 #include "warning_disable.h"
-#define BOOST_SPIRIT_DEBUG
 #include <boost/spirit/include/support_utree.hpp>
 #include <boost/spirit/include/qi.hpp>
