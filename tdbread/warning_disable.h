2fd06236767c9355ff3c8e9094b522bd4a4190e0
Richard Otis <rao140@psu.edu>
2013-03-08 23:20:50 -0500

    Added include guard.

--- tdbread/warning_disable.h
+++ tdbread/warning_disable.h
@@ -1,7 +1,10 @@
 // warning_disable.h -- disabling some excessive compiler warnings
 
+#ifndef INCLUDED_WARNING_DISABLE
+#define INCLUDED_WARNING_DISABLE
 #include <boost/config/warning_disable.hpp>
 #if defined(_MSC_VER)
 #pragma warning (disable: 4503) //  name length exceeded warnings
 #pragma warning (disable: 4244) // "conversion from '__int64' to 'const unsigned int', possible loss of data" in Boost utree lib
+#endif
 #endif
