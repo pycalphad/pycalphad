b9e408b729dbd86d98ad13c3c1accfc66e626df5
Richard Otis <rao140@psu.edu>
2013-05-26 15:28:49 -0400

    Stable commit for new AST construction

---
+++ libgibbs/include/models.hpp
@@ -0,0 +1,100 @@
+/*=============================================================================
+	Copyright (c) 2012-2013 Richard Otis
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
+
+// header for Gibbs energy model structures
+#ifndef INCLUDED_MODELS_HPP
+#define INCLUDED_MODELS_HPP
+
+#include <string>
+#include <boost/spirit/include/support_utree.hpp>
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/composite_key.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+
+namespace models {
+
+using boost::spirit::utree;
+using boost::multi_index_container;
+using namespace boost::multi_index;
+
+struct sublattice_entry {
+	int index; // sublattice index
+	double num_sites; // number of sites
+	std::string phase;
+	std::string species; // species name
+	sublattice_entry (
+			int index_, double num_sites_, std::string phase_, std::string species_) :
+				index(index_), num_sites(num_sites_), phase(phase_), species(species_) {}
+};
+
+struct myindex{};
+struct phases{};
+
+/* Sublattice entries are sorted first by phase name, then index, then by species.
+ * NB: The use of derivation here instead of simple typedef is explained in
+ * Compiler specifics: type hiding.
+ */
+
+struct subl_sort_key:composite_key<
+sublattice_entry,
+BOOST_MULTI_INDEX_MEMBER(sublattice_entry, std::string,phase),
+BOOST_MULTI_INDEX_MEMBER(sublattice_entry,int,index),
+BOOST_MULTI_INDEX_MEMBER(sublattice_entry,std::string,species)
+>{};
+
+
+/* see Compiler specifics: composite_key in compilers without partial
+ * template specialization, for info on composite_key_result_less
+ */
+
+typedef multi_index_container<
+		sublattice_entry,
+		indexed_by<
+		ordered_non_unique<
+		subl_sort_key
+#if defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION)
+		,composite_key_result_less<size_key::result_type>
+#endif
+>,
+ordered_non_unique<
+tag<myindex>,
+BOOST_MULTI_INDEX_MEMBER(sublattice_entry,int,index)
+>,
+ordered_non_unique<
+tag<phases>,
+BOOST_MULTI_INDEX_MEMBER(sublattice_entry,std::string,phase)
+>
+>
+> sublattice_set;
+
+typedef multi_index_container<
+		const sublattice_entry*,
+		indexed_by<
+		ordered_non_unique<
+		subl_sort_key
+#if defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION)
+		,composite_key_result_less<size_key::result_type>
+#endif
+>,
+ordered_non_unique<
+tag<myindex>,
+BOOST_MULTI_INDEX_MEMBER(sublattice_entry,int,index)
+>,
+ordered_non_unique<
+tag<phases>,
+BOOST_MULTI_INDEX_MEMBER(sublattice_entry,std::string,phase)
+>
+>
+> sublattice_set_view;
+
+utree build_Gibbs_ref(const std::string &phasename, const sublattice_set &subl_set);
+
+
+}
+
+#endif
--- libgibbs/source/models/gibbs_ref.cpp
+++ libgibbs/source/models/gibbs_ref.cpp
@@ -1,6 +1,10 @@
 /*=============================================================================
 	Copyright (c) 2012-2013 Richard Otis
 
+	Based on example code from Boost MultiIndex.
+	Copyright (c) 2003-2008 Joaquin M Lopez Munoz.
+	See http://www.boost.org/libs/multi_index for library home page.
+
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
@@ -9,80 +13,109 @@
 
 #include "libgibbs/include/libgibbs_pch.hpp"
 #include "libgibbs/include/optimizer/opt_Gibbs.hpp"
+#include "libgibbs/include/models.hpp"
+#include <string>
 #include <boost/multi_index_container.hpp>
+#include <boost/multi_index/composite_key.hpp>
 #include <boost/multi_index/member.hpp>
 #include <boost/multi_index/ordered_index.hpp>
+#include <boost/tuple/tuple.hpp>
 
 using boost::spirit::utree;
 using boost::multi_index_container;
 using namespace boost::multi_index;
+using namespace models;
 
-struct sublattice_entry {
-	int index; // sublattice index
-	double num_sites; // number of sites
-	std::string species; // species name
-	sublattice_entry (int index_, double num_sites_, std::string species_) :
-		index(index_), num_sites(num_sites_), species(species_) {}
-};
-
-struct myindex{};
-struct species{};
-
-typedef multi_index_container<
-  sublattice_entry,
-  indexed_by<
-    ordered_non_unique<tag<myindex>,BOOST_MULTI_INDEX_MEMBER(sublattice_entry,int,index)>
-    >
-> sublattice_set;
-
-utree build_Gibbs_ref(
-		Sublattice_Collection::const_iterator subl_start,
-		Sublattice_Collection::const_iterator subl_end,
-		const evalconditions &sysstate
+sublattice_set build_variable_map(
+		const Phase_Collection::const_iterator p_begin,
+		const Phase_Collection::const_iterator p_end,
+		const evalconditions &conditions
 		) {
-	utree ret_tree;
-	sublattice_set subl_set;
-	int sublcount = std::distance(subl_start, subl_end);
+	sublattice_set ret_set;
 
-	// Build a structure of all the species in all the sublattices
-	for (auto j = subl_start; j != subl_end; ++j) {
-		for (auto k = (*j).get_species_iterator(); k != (*j).get_species_iterator_end();++k) {
-			// Check if this species in this sublattice is on our list of elements to investigate
-			if (std::find(sysstate.elements.cbegin(),sysstate.elements.cend(),*k) != sysstate.elements.cend()) {
-				int sublindex = std::distance(subl_start,j);
-				double sitecount = (*j).stoi_coef;
-				std::string spec = (*k);
-				subl_set.insert(sublattice_entry(sublindex, sitecount, spec));
+	// All phases
+	for (auto i = p_begin; i != p_end; ++i) {
+		auto subl_start = i->second.get_sublattice_iterator();
+		auto subl_end = i->second.get_sublattice_iterator_end();
+		// All sublattices
+		for (auto j = subl_start; j != subl_end;++j) {
+			// All species
+			for (auto k = (*j).get_species_iterator(); k != (*j).get_species_iterator_end();++k) {
+				// Check if this species in this sublattice is on our list of elements to investigate
+				if (std::find(conditions.elements.cbegin(),conditions.elements.cend(),*k) != conditions.elements.cend()) {
+					int sublindex = std::distance(subl_start,j);
+					double sitecount = (*j).stoi_coef;
+					std::string spec = (*k);
+					std::string phasename = i->first;
+					ret_set.insert(sublattice_entry(sublindex, sitecount, phasename, spec));
+				}
 			}
 		}
 	}
-
-	// iterate over all sublattices
-	// for each sublattice, snag a current permutation of active species in that sublattice
-
-	return ret_tree;
+	return ret_set;
 }
 
-utree permute_site_fractions (const sublattice_set &subl_set, const int &sublindex, const int &sublcount) {
-	/* Create a sublattice view class
-	 * Add to it as we construct a given permutation and pass it as an argument
-	 * This method is GENERAL. We can construct views with multiple species in one sublattice
-	 * Eventually we can make the get_parameter code work by checking if a given view is a permutation
-	 * of one in the parameter database
-	 * If I make the view ordered then I don't even have to check the permutation
-	 * I can do a direct comparison
-	 */
-	if (sublindex == sublcount) {
-		// We are at the end of the recursive loop
-		// Get the corresponding parameter
+utree build_Gibbs_ref(
+		const std::string &phasename,
+		const sublattice_set &subl_set
+		) {
+	utree ret_tree;
+	sublattice_set_view ssv;
+
+	// Get all the sublattices for this phase
+	boost::multi_index::index<sublattice_set,phases>::type::iterator ic0,ic1;
+	boost::tuple::tie(ic0,ic1)=get<phases>(subl_set).equal_range(phasename);
+
+	// Construct a view from the iterators
+	while (ic0 != ic1) {
+		ssv.insert(&*ic0);
+		++ic0;
+	}
+	return permute_site_fractions(ssv, sublattice_set_view(), 0);
+}
+
+utree permute_site_fractions (
+		const sublattice_set_view &total_view, // all sublattices
+		const sublattice_set_view &subl_view, // the active sublattice permutation
+		const int &sublindex,
+		) {
+
+	utree ret_tree;
+
+	// Construct a view of just the current sublattice
+	boost::multi_index::index<sublattice_set_view,myindex>::type::iterator ic0,ic1;
+	boost::tuple::tie(ic0,ic1)=get<myindex>(total_view).equal_range(sublindex);
+
+	if (ic0 == ic1) {
+		/* We are at the bottom of the recursive loop
+		 * or there's an empty sublattice for some reason (bad).
+		 * Use the sublattice permutation to find a matching
+		 * parameter, if it exists.
+		 */
 		return utree(1);
 	}
-	// Get all the species in the sublattice of index sublindex
-	boost::multi_index::index<sublattice_set,myindex>::type::iterator ic0,ic1;
-	boost::tuple::tie(ic0,ic1)=get<myindex>(subl_set).equal_range(sublindex);
 
 	for (auto i = ic0; ic0 != ic1; ++i) {
-		// test
+		sublattice_set_view temp_view = subl_view;
+		utree current_product;
+		utree buildtree;
+		const sublattice_entry *cur = temp_view.insert(&*i); // add ptr to current species to the view
+
+		/* Construct the expression tree.
+		 * Start by building the recursive product of site fractions.
+		 */
+		current_product.push_back("*");
+		// The variable will be represented as a tuple
+		current_product.push_back(boost::make_tuple(cur->phase, cur->index, cur->species));
+		current_product.push_back(
+				permute_site_fractions(total_view, temp_view, sublindex+1)
+				);
+
+		// Contribute this product to the sum
+		buildtree.push_back("+");
+		buildtree.push_back(current_product);
+		buildtree.push_back(ret_tree);
+		ret_tree.swap(buildtree);
 	}
-	return utree();
+	return ret_tree;
 }
