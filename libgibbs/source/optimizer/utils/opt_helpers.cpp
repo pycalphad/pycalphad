639aaf8cd0a6ded968bd45bf55d8086bcd561d68
Richard Otis <rao140@psu.edu>
2013-10-26 19:01:56 -0400

    Unstable. utree-based mole_fraction()

--- libgibbs/source/optimizer/utils/opt_helpers.cpp
+++ libgibbs/source/optimizer/utils/opt_helpers.cpp
@@ -3,9 +3,11 @@
 #include "libgibbs/include/libgibbs_pch.hpp"
 #include "libgibbs/include/optimizer/optimizer.hpp"
 #include "libtdb/include/utils/math_expr.hpp"
+#include <boost/spirit/include/support_utree.hpp>
 #include <math.h>
 #include <iostream>
 
+using boost::spirit::utree;
 
 double mole_fraction(
 	const std::string &spec_name,
@@ -57,6 +59,50 @@ double mole_fraction(
 			return 0;
 		}
 		//std::cout << "mole_fraction = " << numerator << " / " << denominator << " = " << (numerator / denominator) << std::endl;
+		return (numerator / denominator);
+}
+
+boost::spirit::utree mole_fraction(
+	const std::string &phase_name,
+	const std::string &spec_name,
+	const Sublattice_Collection::const_iterator ref_subl_iter_start,
+	const Sublattice_Collection::const_iterator ref_subl_iter_end
+	) {
+		double numerator = 0;
+		double denominator = 0;
+		utree ret_tree;
+
+		for (auto i = ref_subl_iter_start; i != ref_subl_iter_end; ++i) {
+			//std::cout << "sublattice " << std::distance(subl_iter_start,i) << std::endl;
+			const auto sitefrac_begin = i->get_species_iterator();
+			const auto sitefrac_end = i->get_species_iterator_end();
+			const int sublindex = std::distance(ref_subl_iter_start,i);
+			if (std::distance(sitefrac_begin,sitefrac_end) == 0) {
+				// empty sublattice?!
+				BOOST_THROW_EXCEPTION(malformed_object_error() << str_errinfo("Sublattices cannot be empty"));
+			}
+			const double stoi_coef = i->stoi_coef;
+			const auto sitefrac_iter = std::find(sitefrac_begin, sitefrac_end, spec_name);
+			const auto vacancy_iterator = std::find(sitefrac_begin, sitefrac_end, "VA");
+			const bool pure_vacancies =
+					(std::distance(sitefrac_begin,sitefrac_end) == 1 && sitefrac_begin==vacancy_iterator);
+
+			// if the sublattice is pure vacancies, don't include it in the denominator
+			// unless we're calculating mole fraction of VA for some reason
+			if (!pure_vacancies || spec_name == "VA") denominator += stoi_coef;
+			if (sitefrac_iter != sitefrac_end) {
+				utree temp_tree;
+				std::stringstream ss;
+				ss << phase_name << "_" << sublindex << "_" << spec_name;
+				temp_tree.push_back("*");
+				temp_tree.push_back(stoi_coef);
+				temp_tree.push_back(ss.str());
+				const double num = stoi_coef * sitefrac_iter->second;
+
+				numerator += num;
+			}
+		}
+
 		return (numerator / denominator);
 }
 
