c025dcd3ad01f423a4bd0ddbed0ef2a114f89ed5
Richard Otis <rao140@psu.edu>
2013-10-26 14:40:18 -0400

    Stub

---
+++ libgibbs/source/optimizer/constraints/sublatticebalanceconstraint.cpp
@@ -0,0 +1,13 @@
+/*=============================================================================
+	Copyright (c) 2012-2013 Richard Otis
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
+
+// definition of SublatticeBalanceConstraint class
+
+#include "libgibbs/include/libgibbs_pch.hpp"
+#include "libgibbs/include/constraint.hpp"
+
+using boost::spirit::utree;
