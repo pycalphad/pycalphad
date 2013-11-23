1540ff0ffc6feb1ee3e611e75b568a8fb4d50129
Richard Otis <rao140@psu.edu>
2013-11-23 11:45:31 -0500

    Some initial construction code for CompositionSet, not yet plugged into GibbsOpt. The result of a new keyword should always be wrapped inside of a named smart pointer to avoid memory leaks.

--- libgibbs/include/compositionset.hpp
+++ libgibbs/include/compositionset.hpp
@@ -10,14 +10,30 @@
 #ifndef COMPOSITIONSET_INCLUDED
 #define COMPOSITIONSET_INCLUDED
 
-#include "libtdb/include/structure.hpp"
 #include "libgibbs/include/models.hpp"
+#include "libtdb/include/structure.hpp"
+#include <memory>
 
-// A CompositionSet is a specialization of libtdb's Phase class
+// A CompositionSet works with libtdb's Phase class
 // Its purpose is to handle the optimizer's specific configuration for the given conditions and models
 // Multiple CompositionSets of the same Phase can be created to handle miscibility gaps
-class CompositionSet : Phase {
-	std::vector<EnergyModel> models;
+class CompositionSet {
+public:
+	std::vector<double> evaluate_objective();
+	std::vector<double> evaluate_objective_gradient();
+	std::vector<double> evaluate_constraints();
+	std::vector<double> evaluate_jacobian_of_constraints();
+	std::vector<double> evaluate_hessian();
+	std::string name() { return cset_name; }
+	// make CompositionSet from existing Phase
+	CompositionSet(const Phase& phaseobj, const parameter_set &pset, const sublattice_set &sublset);
+private:
+	std::string cset_name;
+	std::map<std::string,std::unique_ptr<EnergyModel>> models;
+	// Block copy ctor
+	CompositionSet(const CompositionSet&);
+	// Block assignment operator
+	CompositionSet& operator=(const CompositionSet&);
 };
 
 
--- libgibbs/source/optimizer/compositionset.cpp
+++ libgibbs/source/optimizer/compositionset.cpp
@@ -9,3 +9,14 @@
 
 #include "libgibbs/include/libgibbs_pch.hpp"
 #include "libgibbs/include/compositionset.hpp"
+
+CompositionSet::CompositionSet(const Phase &phaseobj, const parameter_set &pset, const sublattice_set &sublset) {
+	cset_name = phaseobj.name();
+
+	// Now initialize the appropriate models
+	models["PURE_ENERGY"] = std::unique_ptr<EnergyModel>(new PureCompoundEnergyModel(phaseobj.name(), sublset, pset));
+	models["IDEAL_MIX"] = std::unique_ptr<EnergyModel>(new IdealMixingModel(phaseobj.name(), sublset));
+	models["REDLICH_KISTER"] = std::unique_ptr<EnergyModel>(new RedlichKisterExcessEnergyModel(phaseobj.name(), sublset, pset));
+	models["IHJ_MAGNETIC"] = std::unique_ptr<EnergyModel>(new IHJMagneticModel(phaseobj.name(), sublset, pset,
+					phaseobj.magnetic_afm_factor, phaseobj.magnetic_sro_enthalpy_order_fraction));
+}
