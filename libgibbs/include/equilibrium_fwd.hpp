9716de92ff194ec43d8cb3610e6a0007964aa5ec
Richard Otis <rao140@psu.edu>
2013-12-01 13:24:13 -0500

    Forward declarations

---
+++ libgibbs/include/equilibrium_fwd.hpp
@@ -0,0 +1,16 @@
+/*=============================================================================
+	Copyright (c) 2012-2013 Richard Otis
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
+
+// forward declaration of Equilibrium
+
+#ifndef INCLUDED_OPT_GIBBS_FWD
+#define INCLUDED_OPT_GIBBS_FWD
+
+class Equilibrium;
+
+
+#endif
