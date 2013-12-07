2ae16829d62556d938ea415971ab4d0d3f97fafe
Richard Otis <rao140@psu.edu>
2013-12-07 14:44:16 -0500

    remove obsolete stub code

--- libgibbs/source/models/excess_ternary_muggianu.cpp
+++
@@ -1,8 +0,0 @@
-/*=============================================================================
-	Copyright (c) 2012-2013 Richard Otis
-
-    Distributed under the Boost Software License, Version 1.0. (See accompanying
-    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-=============================================================================*/
-
-// Model for excess Gibbs energy ternary interaction parameters after Muggianu, as reported by Hillert (1980)
