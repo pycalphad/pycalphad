d394fe3640cf7a685349cded668ccfac5399cabb
Richard Otis <rao140@psu.edu>
2013-12-15 16:48:04 -0500

    Initial attempt at N-dimensional grid generation

---
+++ libgibbs/include/optimizer/utils/ndgrid.hpp
@@ -0,0 +1,51 @@
+/*=============================================================================
+	Copyright (c) 2012-2013 Richard Otis
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
+
+// N-Dimensional uniform grid
+
+#ifndef INCLUDED_NDGRID
+#define INCLUDED_NDGRID
+
+#include <vector>
+#include <numeric>
+
+struct NDGrid {
+	template <typename Func> static void apply(
+			const double& min_extent,
+			const double& max_extent,
+			const std::size_t& dimension,
+			const unsigned int& grid_points_per_major_axis,
+			const Func &func,
+			std::vector<double>& address) {
+		if (address.size() == dimension) {
+			// terminating condition; address is complete
+			func(address);
+		}
+		else {
+			double step = (max_extent - min_extent) / (double)_grid_points_per_major_axis;
+			for (auto j = 0; j <= grid_points_per_major_axis; ++j) {
+				double location = step*j + min_extent;
+				address.push_back(location);
+				if (std::accumulate(temp.begin(), temp.end()) > 1) break; // addresses must sum to 1
+				// recursive step
+				this->apply(min_extent, max_extent, dimension, grid_points_per_major_axis, func, temp);
+				address.pop_back(); // remove the element we just added (this way avoids copying)
+			}
+		}
+	}
+	template <typename Func> static inline void apply(
+			const double& min_extent,
+			const double& max_extent,
+			const std::size_t& dimension,
+			const unsigned int& grid_points_per_major_axis,
+			const Func &func) {
+		std::vector<double> address;
+		this->apply(min_extent_max_extent, dimension, grid_points_per_major_axis, func, address);
+	}
+};
+
+#endif
