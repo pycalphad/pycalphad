5d02647a4e2f8af5aac71d978aa65899793e2640
Richard Otis <rao140@psu.edu>
2013-12-15 20:19:40 -0500

    Design notes for LocateMinima

--- libgibbs/source/optimizer/utils/ezd_minimization.cpp
+++ libgibbs/source/optimizer/utils/ezd_minimization.cpp
@@ -17,7 +17,25 @@
 
 namespace Optimizer {
 
+// TODO: Should this be a member function of GibbsOpt?
+// The function calling LocateMinima definitely should be at least (needs access to all CompositionSets)
 void LocateMinima(std::unique_ptr<CompositionSet> const &phase, const int depth) {
+	// Because the grid is uniform, we can assume that each point is the center of an N-cube
+	// of width max_extent-min_extent. Boundary points are a special case.
+	// Do whatever normalization is needed to put them in the feasible region
+	//
+	// EZD Global Minimization (Emelianenko et al., 2006)
+	// For initial depth (1): FIND CONCAVITY REGIONS
+	//	  Sample some points on the domain using NDGrid (probably separate function)
+	//    Calculate G'' for all sampled points
+	//    Save all points for which G'' > 0
+	//    For each saved point, send to next depth
+	// For depth > 1: FIND MINIMA
+	//    Sample some points on the domain using NDGrid (probably separate function)
+	//    Calculate G' for all sampled points
+	//    Find the point (z) with the minimum magnitude of G'
+	//    If that magnitude is less than some defined epsilon, or we've hit max_depth, return z as a minimum
+	//    Else use N-cube assumption to define new domain around z and send to next depth (return minima from that)
 
 }
 
