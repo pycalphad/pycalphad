d943e6afdd0ca192dfe63e6a1265c5238ad781ac
Richard Otis <rao140@psu.edu>
2014-01-13 21:57:23 -0500

    Remove Domain object

--- libgibbs/include/optimizer/utils/ezd_minimization.hpp
+++ libgibbs/include/optimizer/utils/ezd_minimization.hpp
@@ -20,10 +20,7 @@
 #include <vector>
 
 namespace Optimizer {
-template <typename T> struct Domain {
-	std::vector<std::vector<T> > lower_left_corner;
-	std::vector<std::vector<T> > upper_right_corner;
-};
+
 void LocateMinima(
 		CompositionSet const &phase,
 		sublattice_set const &sublset,
