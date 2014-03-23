ce0937bde74a8bf7980c5e984d93b86a34751a8b
Richard Otis <richard.otis@outlook.com>
2014-03-23 19:24:30 -0400

    Support for finding orientation of a point relative to a hyperplane.

--- libgibbs/include/optimizer/utils/geometry/orientation.hpp
+++ libgibbs/include/optimizer/utils/geometry/orientation.hpp
@@ -9,16 +9,36 @@
 
 #ifndef INCLUDED_ORIENTATION
 #define INCLUDED_ORIENTATION
-#include "libgibbs/include/optimizer/utils/ndsimplex.hpp"
+#include "libgibbs/include/utils/determinant.hpp"
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/matrix_proxy.hpp>
+#include <boost/assert.hpp>
 
 /* The orientation is the signed volume of the simplex spanned by the hyperplane
  * and the candidate point, up to a constant factor depending on the dimension of
  * the space.
  * Reference: Computing in Euclidean Geometry, 2nd ed., 1995, edited by Ding-Zhu Du and Frank Hwang
- * Here the vertices of an NDSimplex are used to define the hyperplane.
+ * 
  */
-template <typename T>
-int orientation ( const NDSimplex &simplex , const T &candidate_point ) {
-    return 0;
+template <typename VectorType, typename DataType>
+double orientation ( const boost::numeric::ublas::matrix<DataType> &hyperplane_points , 
+                  const VectorType &candidate_point ) {
+    using namespace boost::numeric::ublas;
+    BOOST_ASSERT ( candidate_point.size() == hyperplane_points.size2() );
+    matrix<DataType> orientation_test_matrix (hyperplane_points.size1()+1, hyperplane_points.size2()+1);
+    matrix_row<DataType> last_row ( orientation_test_matrix, orientation_test_matrix.size1() );
+    matrix_column<DataType> last_column ( orientation_test_matrix, orientation_test_matrix.size2() );
+    // Fill the last column with 1's
+    for (auto i = 0; i < orientation_test_matrix.size1(); ++i) {
+        last_column ( i ) = 1;
+    }
+    // Fill the upper left part of the test matrix with the hyperplane points
+    subrange ( orientation_test_matrix, 0,hyperplane_points.size1(), 0,hyperplane_points.size2() ) = hyperplane_points;
+    // Fill the last row with the coordinates of the candidate point
+    for ( auto i = candidate_point.cbegin(); i != candidate_point.cend(); ++i) {
+        last_row ( std::distance( candidate_point.cbegin(), i ) ) = *i;
+    }
+    // Calculate and return the determinant of the orientation test matrix
+    return determinant ( orientation_test_matrix );
 }
 #endif
