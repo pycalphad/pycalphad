55f016337ba08acc31d5a3f774c9422179a76559
Richard Otis <richard.otis@outlook.com>
2014-03-23 16:11:57 -0400

    Stub code for lower convex hull calculation.

---
+++ libgibbs/include/optimizer/utils/convex_hull.hpp
@@ -0,0 +1,20 @@
+/*=============================================================================
+ Copyright (c) 2012-2014 Richard Otis
+ 
+ Distributed under the Boost Software License, Version 1.0. (See accompanying
+ file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ =============================================================================*/
+
+// Calculation of the lower convex hull of a set of points
+// Modified QuickHull algorithm using d-dimensional Beneath-Beyond
+// Reference: N. Perevoshchikova, et al., 2012, Computational Materials Science.
+// "A convex hull algorithm for a grid minimization of Gibbs energy as initial step 
+//    in equilibrium calculations in two-phase multicomponent alloys"
+
+
+namespace Optimizer {
+    namespace details {
+        void lower_convex_hull ( ) {
+        }
+    }
+}
