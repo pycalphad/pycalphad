8707bdd88428b13dbb692270d6b07782a68283dd
Richard Otis <richard.otis@outlook.com>
2014-05-08 16:50:54 -0400

    Implementation of Optimizer::details::ConvexHullMap.

--- libgibbs/include/optimizer/utils/hull_mapping.hpp
+++ libgibbs/include/optimizer/utils/hull_mapping.hpp
@@ -11,11 +11,25 @@
 #define INCLUDED_HULL_MAPPING
 
 #include <boost/noncopyable.hpp>
-#include <forward_list>
-#include <set>
+#include <map>
+#include <string>
 #include <vector>
 
 namespace Optimizer { namespace details {
+
+/* This class associates important metadata with each point on the convex hull
+ * of a given phase. This way we always remember the internal degrees of freedom
+ * that are contained within a phase.
+ */
+template <typename CoordinateType = double, typename EnergyType = CoordinateType> 
+struct ConvexHullEntry {
+    typedef std::vector<CoordinateType> PointType;
+    typedef std::map<std::string,CoordinateType> GlobalPointType;
+    std::string phase_name;
+    CoordinateType energy;
+    PointType internal_coordinates;
+    GlobalPointType global_coordinates;
+};
 /* The purpose of ConvexHullMap is to keep track of all of the internal convex
  * hulls of all of the phases during global minimization. The points of each phase's
  * internal hull are mapped to the global composition space. Then a global hull is
@@ -25,22 +39,18 @@ namespace Optimizer { namespace details {
  * internal degrees of freedom; this is the composition of that phase. Using the constraints
  * we fix a point inside the facet and use the lever rule to find the phase fractions.
  */
-template <typename CoordinateType>
+template <typename CoordinateType = double, typename EnergyType = CoordinateType>
 class ConvexHullMap : private boost::noncopyable {
 public:
-    typedef std::vector<CoordinateType> PointType;
-    // Can't use vector here due to iterator invalidation on container resize
-    typedef std::forward_list<const PointType> PointContainerType;
-    typename PointContainerType::const_iterator find_internal_point_from_global_id ( const std::size_t index ) const;
-
-    void add_point ( const PointType &internal_coordinates, const PointType &global_coordinates );
+    typedef ConvexHullEntry<CoordinateType,EnergyType> HullEntryType;
+    typedef typename HullEntryType::PointType PointType;
+    typedef typename HullEntryType::GlobalPointType GlobalPointType;
+    auto find_entry_from_global_id ( const std::size_t index ) -> typename HullEntryType::const_iterator const;
+    void insert_point ( const std::string &phase_name, const EnergyType &energy, 
+                        const PointType &internal_coordinates, const GlobalPointType &global_coordinates );
 private:
-    // These two must be manually kept in sync using point IDs!
-    PointContainerType hull_phase_internal_points;
-    PointContainerType hull_global_points;
-    // using set::lower_bound(), can get iterator to start/end of phase internal points
-    // the difference between the actual value of the key and the queried value gives the offset
-    std::set<std::size_t,typename PointContainerType::const_iterator> phase_index_bounds;
+    // entries should be inserted in global ID order
+    std::vector<const HullEntryType> all_points;
 };
 
 } // namespace details
--- libgibbs/source/optimizer/utils/hull_mapping.cpp
+++ libgibbs/source/optimizer/utils/hull_mapping.cpp
@@ -11,17 +11,32 @@
 #include "libgibbs/include/optimizer/utils/hull_mapping.hpp"
 
 namespace Optimizer { namespace details {
-template <typename ValueType>
-void ConvexHullMap<ValueType>::add_point ( 
-        const ConvexHullMap<ValueType>::PointType &internal_coordinates, 
-        const ConvexHullMap<ValueType>::PointType &global_coordinates ) {
-    hull_phase_internal_points.insert_after ( hull_phase_internal_points.end(), internal_coordinates );
-    hull_global_points.insert_after ( hull_global_points.end(), global_coordinates );
+/*
+ * Add a point from a phase's internal convex hull to the global point map.
+ */
+template <typename CoordinateType, typename EnergyType>
+void ConvexHullMap<CoordinateType,EnergyType>::insert_point ( 
+    const std::string &phase_name,
+    const EnergyType &energy, 
+    const PointType &internal_coordinates,
+    const GlobalPointType &global_coordinates
+) {
+    HullEntryType hull_entry;
+    hull_entry.energy = energy;
+    hull_entry.internal_coordinates = internal_coordinates;
+    hull_entry.global_coordinates = global_coordinates;
+    all_points.emplace_back ( std::move ( hull_entry ) );
 }
-template <typename ValueType>
-typename ConvexHullMap<ValueType>::PointContainerType::const_iterator 
- ConvexHullMap<ValueType>::find_internal_point_from_global_id ( const std::size_t index ) const {
-     
+
+/* 
+ * Locate the entry corresponding to this global point ID.
+ */
+template <typename CoordinateType, typename EnergyType>
+auto ConvexHullMap<CoordinateType,EnergyType>::find_entry_from_global_id ( const std::size_t index ) 
+-> typename HullEntryType::const_iterator const {
+     auto entry_iterator = all_points.cbegin();
+     std::advance ( entry_iterator, index );
+     return entry_iterator;
  }
 } // namespace details
 } // namespace Optimizer
