bd613cceb694c8c309f9c8b9ec2db99f91a65bec
Richard Otis <richard.otis@outlook.com>
2014-05-17 18:49:49 -0400

    Initial attempt at architecture for libtdbcpp.

---
+++ calphad/io/database.py
@@ -0,0 +1,11 @@
+import calphad.io.libtdbcpp as ctdb
+
+class Database():
+	def load(self,source='',type=DatabaseTDB):
+		pass
+	pass
+
+class DatabaseTDB(Database):
+	def __init__(self,source=''):
+		self.internal_database_object = []
+	pass
