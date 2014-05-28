205748f0ec61b7e93a913f43991eafa4d96a54cf
Richard Otis <richard.otis@outlook.com>
2014-05-27 23:14:51 -0400

    More test code for Python-based global minimization.

--- examples/varmap.py
+++ examples/varmap.py
@@ -1,6 +1,7 @@
 import calphad.libcalphadcpp as lcp
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 
 class GlobMin(lcp.GlobalMinimizer):
    def triangulate_hull(self):
@@ -8,15 +9,17 @@ class GlobMin(lcp.GlobalMinimizer):
        facets = lcp.GlobalMinimizer.get_facets(self)
        entries = []
        facet_list = []
+       point_phases = []
        for entry in raw_entries:
 	       stripped_entry = []
 	       for coords in entry.global_coordinates:
 		       stripped_entry.append(coords.data())
 	       #stripped_entry.append(entry.energy)
+	       point_phases.append(entry.phase_name)
 	       entries.append(stripped_entry)
        for facet in facets:
 	       facet_list.append(facet.vertices)
-       return entries, facet_list
+       return entries, facet_list, point_phases
 
 
 # Load thermodynamic database
@@ -26,7 +29,7 @@ if maindb == None:
 
 # Set equilibrium conditions
 conds = lcp.evalconditions()
-conds.statevars['T'] = 300
+conds.statevars['T'] = 2500
 conds.statevars['P'] = 101325
 conds.statevars['N'] = 1
 conds.elements.append("FE")
@@ -53,21 +56,36 @@ print ("Starting global minimization")
 globminengine = GlobMin(cmps, varmap, conds)
 
 print("Getting hull")
-xy, triangles = globminengine.triangulate_hull()
+xy, triangles, point_phases = globminengine.triangulate_hull()
 xy = np.asarray(xy)
 x = xy[:,0]
 y = xy[:,1]
 z = 1 - x - y
-new_x = x
-new_y = y
-#new_x = 0.5*(2*y + z)/(x+y+z)
-#new_y = (np.sqrt(3)/2)*z/(x+y+z)
+
+new_x = 0.5*(2*y + z)/(x+y+z)
+new_y = (np.sqrt(3)/2)*z/(x+y+z)
 triangles = np.asarray(triangles)
-plt.figure()
+colorlist = {}
+import colorsys
+N = len(conds.phases)
+HSV_tuples = [(x*1.0/N, 0.5, 0.5) for x in range(N)]
+RGB_tuples = map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples)
+
+phasecount = 0
+for phase in conds.phases:
+	colorlist[phase.key()] = RGB_tuples[phasecount]
+	phasecount = phasecount + 1
+
+plotcolors = map(lambda x: colorlist[x], point_phases)
+
+colormap = LinearSegmentedColormap('PhaseColor', plotcolors)
+
+plt.figure(dpi=600)
 plt.xlim([-0.01,1])
 plt.ylim([-0.01,1])
 plt.gca().set_aspect('equal')
-plt.triplot(new_x, new_y, triangles, 'go-')
+plt.triplot(new_x, new_y, triangles, 'go-', marker=None, zorder=1)
+plt.scatter(new_x, new_y, cmap=colormap, marker='.', zorder=2)
 plt.title('triplot of user-specified triangulation')
 plt.xlabel('X(A)')
 plt.ylabel('X(B)')
