a541d51ad2ee929ff5e9e7e0e4e50f13d1c23d1a
Richard Otis <richard.otis@outlook.com>
2014-06-08 15:00:47 -0400

    More docstrings for GlobalMinimizer.

--- calphad/minimize/globmin.py
+++ calphad/minimize/globmin.py
@@ -22,21 +22,65 @@ class GlobalMinimizer(lcp.GlobalMinimizer):
 		
 	"""
 	def __init__(self, phase_dict, sublset, conditions, **kwargs):
-		# Initialize minimizer
+		"""
+		Construct the convex hull to find the minimum energy surface.
+		
+		Parameters
+		----------
+		phase_dict : dict
+		    Dictionary of CompositionSet objects.
+		sublset : sublattice_set
+		    Sublattice configuration of all phases.
+		conditions : evalconditions
+		critical_edge_length : double, optional
+		    Minimum length of a tie line. (default is 0.005)
+		initial_subdivisions_per_axis : int, optional
+		    Number of axis subdivisions along each axis. (default is 20)
+		refinement_subdivisions_per_axis : int, optional
+		    Number of axis subdivisions during mesh refinement. (default is 2)
+		max_search_depth : int, optional
+		    Maximum recursive depth for mesh refinement. (default is 5)
+		    
+		
+		Examples
+		--------
+		TODO: None yet.
+		"""
+		# Initialize minimizer on C++ side
 		lcp.GlobalMinimizer.__init__(self)
+		# Create list of allowed options
 		allowed_opts = ('critical_edge_length',
 	                   'initial_subdivisions_per_axis',
 	                   'refinement_subdivisions_per_axis',
 	                   'max_search_depth'
 	                   )
-		# Set options
+		# Set options; these will override C++ settings
 		for k, v in kwargs.iteritems():
 			assert( k in allowed_opts )
 			setattr(self, k, v)
 		
-		# Execute calculation
+		# Execute calculation on C++ side
 		lcp.GlobalMinimizer.run(self, phase_dict, sublset, conditions)
 	def triangulate_hull(self):
+		"""
+		Get the triangulation of the convex hull.
+		
+		Parameters
+		----------
+		None.
+		
+		Returns
+		-------
+		entries : ordered list of points on the convex hull for all phases
+		facet_list : ordered list of vertices comprising each facet
+		point_phases : ordered list of the phase where each point comes from
+		hull_mask : ordered list of bools; true if _not_ on global hull
+		    
+		See Also
+		--------
+		Nothing.
+
+		"""
 		raw_entries = lcp.GlobalMinimizer.get_hull_entries(self)
 		facets = lcp.GlobalMinimizer.get_facets(self)
 		entries = []
