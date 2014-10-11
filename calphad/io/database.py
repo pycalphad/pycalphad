e618e29700e80cce9f0997bddbab66a8d7bb9909
Richard Otis <richard.otis@outlook.com>
2014-10-11 17:43:52 -0400

    Initial attempt at restructuring database module in Pure Python. Introduces Database, Parser, Model concepts.

--- calphad/io/database.py
+++ calphad/io/database.py
@@ -1,167 +1,252 @@
 """The database module provides support for reading and writing data types
-associated with structured thermodynamic data.
+associated with structured thermodynamic/kinetic data.
+"""
+from sets import Set
+from tinydb import TinyDB
+
+class Database:
+    """
+    Structured thermodynamic and/or kinetic data.
+    Databases are usually filled by Parsers and read by Models.
+
+    Attributes
+    ----------
+    None yet.
+
+    Methods
+    -------
+    None yet.
+
+    """
+    def __init__(self):
+        self._elements = Set()
+        self._species = Set()
+        self._phases = {}
+        self._structure_dict = {} # System-local phase names to global IDs
+        self._parameters = TinyDB()
+        self._symbols = {}
+        self._references = {}
+        # Note: No typedefs here (from TDB files)
+        # Instead we put that information in the model_hint for _phases
+    def add_element(self, element):
+        """
+        Add an element.
+
+        Parameters
+        ----------
+        element : string
+            Name of the element.
+
+        Examples
+        --------
+        >>>> db = Database()
+        >>>> db.add_element('Al')
+        """
+        if not isinstance(element, str):
+            raise TypeError
+        self._elements.add(element.upper())
+    def add_species(self, species):
+        """
+        Add a species.
+
+        Parameters
+        ----------
+        species : string
+            Name of the species.
+
+        Examples
+        --------
+        >>>> db = Database()
+        >>>> db.add_species('CO2')
+        """
+        if not isinstance(species, str):
+            raise TypeError
+        # TODO: Verify that species def is all defined elements
+        self._species.add(species.upper())
+    def add_symbol(self, name, symbol):
+        """
+        Add a symbol. This is a FUNCTION in Thermo-Calc lingo.
+
+        Parameters
+        ----------
+        name : string
+            Name of the symbol.
+        symbol : object
+            Abstract representation of symbol, e.g., in SymPy format.
+
+        Examples
+        --------
+        None yet.
+        """
+        self._symbols[name.upper()] = symbol
+    def add_reference(self, name, reference):
+        """
+        Add a reference to a source of information.
+
+        Parameters
+        ----------
+        name : string
+            Unique name for the reference.
+        reference : string
+            A citation to a source of information.
+
+        Examples
+        --------
+        None yet.
+        """
+        self._references[name.upper()] = reference
+    def add_parameter(self, phase_name, constituent_array, param_type,
+                      param_order, param, ref=None):
+        """
+        Add a parameter.
+
+        Parameters
+        ----------
+        name : string
+            Name of the symbol.
+        symbol : object
+            Abstract representation of symbol, e.g., in SymPy format.
+
+        Examples
+        --------
+        None yet.
+        """
+        if not isinstance(phase_name, str):
+            raise TypeError
+        if not isinstance(param_order, int):
+            raise TypeError
+        if not isinstance(param_type, str):
+            raise TypeError
+        if not isinstance(ref, str):
+            raise TypeError
+        if not isinstance(constituent_array, list):
+            raise TypeError
+        new_parameter = {
+            'phase_name': phase_name,
+            'constituent_array': constituent_array,
+            'parameter_type': param_type,
+            'parameter': param,
+            'reference': ref
+        }
+        param_id = self._parameters.insert(new_parameter)
+        return param_id
+    def add_phase(self, phase_name, constituents, model_hints):
+        """
+        Add a phase.
+
+        Parameters
+        ----------
+        phase_name : string
+            System-local name of the phase.
+        constituents : list
+            Possible phase constituents (elements and/or species).
+        model_hints : list
+            Structured "hints" for a Model trying to read this phase.
+            Hints for major constituents and typedefs (Thermo-Calc) go here.
+
+        Examples
+        --------
+        None yet.
+        """
+        raise NotImplementedError
+    def search(self, query):
+        """
+        Search for parameters matching the specified query.
+
+        Parameters
+        ----------
+        query : object
+            Structured database query in TinyDB format.
+
+        Examples
+        --------
+        >>>> from tinydb import where
+        >>>> db = Database()
+        >>>> eid = db.add_parameter(...) #TODO
+        >>>> db.search(where('eid') == eid)
+        """
+        return self._parameters.search(query)
+    def to_json(self):
+        """
+        Serialize Database to espei JSON format.
+        """
+        raise NotImplementedError
+    def to_tdb(self):
+        """
+        Serialize Database to Thermo-Calc TDB format.
+        """
+        raise NotImplementedError
+    def from_json(self, input_json):
+        """
+        Construct Database from espei JSON format.
+
+        Parameters
+        ----------
+        input_json : string
+            Raw data or path to JSON file.
+        """
+        raise NotImplementedError
+    def from_tdb(self, input_data):
+        """
+        Construct Database from Thermo-Calc TDB format.
+
+        Parameters
+        ----------
+        input_data : string
+            Raw data or path to TDB file.
+        """
+        raise NotImplementedError
+
+
+class TDBParser(object):
+    """
+    Structured thermodynamic data in TDB format.
+
+    Attributes
+    ----------
+    None.
+
+    Methods
+    -------
+    None.
+    """
+    def __init__(self, input_data, database):
+        """
+        Load a thermodynamic database (TDB).
+
+        Parameters
+        ----------
+        input_data : string
+            Raw data or path to TDB file.
+        database : Database
+            Database to fill.
+        """
+        print(input_data)
 
 """
-import calphad.libcalphadcpp as lcp
-from itertools import ifilterfalse, tee
-
-class Database():
-	"""
-	Structured thermodynamic data.
-	
-	Attributes
-	----------
-	type : string
-	    Format of the source data.
-	    
-	Methods
-	-------
-	raw_command(command)
-	    Execute a raw command on the data.
-	    
-	"""
-	def raw_command(self,command):
-		"""
-		Execute a raw command on the data.
-		The effects depend on the specifics of the implementation.
-		
-		Parameters
-		----------
-		command : Command string
-		
-		Returns
-		-------
-		Nothing.
-		    
-		See Also
-		--------
-		Nothing.
-
-		"""
-		pass
-	pass
-
-class TDB(Database):
-	"""
-	Structured thermodynamic data in TDB format.
-	
-	Attributes
-	----------
-	type : {'thermocalc'}
-	    Storage format of the source database.
-	    
-	Methods
-	-------
-	raw_command(command)
-	    Send a raw command to the parser.
-	    
-	"""
-	def __init__(self,filepath='',type='thermocalc'):
-		"""
-		Load a thermodynamic database (TDB).
-		
-		Parameters
-		----------
-		filepath : string, optional
-		    Path to TDB file.
-		type : {'thermocalc'}, optional
-		    Storage format of the source database.
-		
-		Examples
-		--------
-		>>> emptytdb = calphad.io.database.TDB()
-		>>> mytdb = calphad.io.database.TDB('feconi.tdb')
-		
-		"""
-		self._database = lcp.Database()
-		self.type = type
-		if (filepath != ''):
-			self._database = lcp.Database(filepath) # temporary
-			"""
-			handle = open(filepath,'r')
-			lines = handle.read() # read entire file
-			handle.close()
-			# Take care of tabs, extra whitespace
-			lines = lines.replace('\t',' ')
-			lines = lines.strip()
-			# Split the string by newlines
-			splitlines = lines.split('\n')
-			# Remove extra whitespace inside line
-			splitlines = [' '.join(k.split()) for k in splitlines]
-			# Remove comments
-			splitlines = [k for k in splitlines if not k.startswith("$")]
-			# Combine everything back together
-			lines = ' '.join(splitlines)
-			# Now split by the command delimeter
-			commands = lines.split('!')
-			# Filter out comments one more time
-			# It's possible they were at the end of a command
-			commands = [k for k in commands if not k.startswith("$")]
-			# Separate out all PARAMETER commands; to be executed last
-			commands,para_commands = partition(
-				lambda cmd: cmd.upper().startswith("PARA"), 
-				commands)
-			map(self.raw_command,commands)
-			map(self.raw_command,para_commands)
-			"""
-	def get_info(self):
-		"""
-		Get the database's human-readable information (if any).
-		
-		Parameters
-		----------
-		None.
-		
-		Returns
-		-------
-		Human-readable information about the database.
-		
-		See Also
-		--------
-		Nothing.
-		
-		Examples
-		--------
-		>>> mytdb = calphad.io.database.TDB()
-		>>> mytdb.get_info()
-		
-		"""
-		return self._database.get_info()
-	def raw_command(self,command):
-		"""
-		Send a raw command to the TDB parser.
-		All commands passed this way must be self-contained (i.e., terminated).
-		The order in which commands are passed matters!
-		
-		Parameters
-		----------
-		command : Command string with terminator (e.g., '!' in Thermo-Calc)
-		
-		Returns
-		-------
-		Nothing.
-		    
-		See Also
-		--------
-		Nothing.
-		
-		Examples
-		--------
-		>>> mytdb = calphad.io.database.TDB()
-		>>> mytdb.raw_command("ELEMENT AL FCC_A1 2.6982E+01 4.5773E+03  2.8322E+01!")
-		>>> mytdb.raw_command("TYPE_DEFINITION % SEQ *!")
-		>>> mytdb.raw_command("PHASE LIQUID:L %  1  1.0  !")
-		>>> mytdb.raw_command("FUNCTION  GALHCP   298.0  +5481-1800*T;  6000  N !")
-		>>> mytdb.raw_command("PARAMETER G(LIQUID,AL;0) 298.15 +10465.5-3.39259*T; 6000 N !")
-		
-		"""
-		print(command)
-		self._database.process_command(command)
-		pass
-	pass
-
-def partition(pred, iterable):
-    'Use a predicate to partition entries into false entries and true entries'
-    # partition(is_odd, range(10)) --> 0 2 4 6 8   and  1 3 5 7 9
-    t1, t2 = tee(iterable)
-    return ifilterfalse(pred, t1), filter(pred, t2)
+            handle = open(filepath,'r')
+            lines = handle.read() # read entire file
+            handle.close()
+            # Take care of tabs, extra whitespace
+            lines = lines.replace('\t',' ')
+            lines = lines.strip()
+            # Split the string by newlines
+            splitlines = lines.split('\n')
+            # Remove extra whitespace inside line
+            splitlines = [' '.join(k.split()) for k in splitlines]
+            # Remove comments
+            splitlines = [k for k in splitlines if not k.startswith("$")]
+            # Combine everything back together
+            lines = ' '.join(splitlines)
+            # Now split by the command delimeter
+            commands = lines.split('!')
+            # Filter out comments one more time
+            # It's possible they were at the end of a command
+            commands = [k for k in commands if not k.startswith("$")]
+            # Separate out all PARAMETER commands; to be executed last
+            commands,para_commands = partition(
+                lambda cmd: cmd.upper().startswith("PARA"), 
+                commands)
+            map(self.raw_command,commands)
+            map(self.raw_command,para_commands)
+            """
