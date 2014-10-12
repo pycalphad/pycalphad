20a57d7076c4eaeb7b6fd2184446785f9b652504
Richard Otis <richard.otis@outlook.com>
2014-10-11 21:48:24 -0400

    Reorganize file import for tdbread()

--- calphad/io/database.py
+++ calphad/io/database.py
@@ -214,30 +214,34 @@ class Database:
         input_data : string
             Raw data or path to TDB file.
         """
-        raise NotImplementedError
+        if input_data.find('\n') == -1:
+            # This is probably a filename rather than a raw TDB
+            raw_data = None
+            with open(input_data, 'r') as data:
+                raw_data = data.read()
+            tdbread(self, raw_data)
+        else:
+            tdbread(self, input_data)
 
-from itertools import filterfalse, tee
+from itertools import tee
 
 def partition(pred, iterable):
     'Use a predicate to partition entries into false entries and true entries'
     # partition(is_odd, range(10)) --> 0 2 4 6 8   and  1 3 5 7 9
     tee1, tee2 = tee(iterable)
-    return filterfalse(pred, tee1), filter(pred, tee2)
+    return [i for i in tee1 if not pred(i)], [i for i in tee2 if pred(i)]
 
-def tdbread(lines):
+def tdbread(targetdb, lines):
     """
     Parse a TDB file into a pycalphad Database object.
 
     Parameters
     ----------
+    targetdb : Database
+        A pycalphad Database.
     lines : string
         A raw TDB file.
-
-    Returns
-    -------
-    Database
     """
-    newdb = Database()
     lines = lines.replace('\t', ' ')
     lines = lines.strip()
     # Split the string by newlines
@@ -253,8 +257,14 @@ def tdbread(lines):
     # Filter out comments one more time
     # It's possible they were at the end of a command
     commands = [k for k in commands if not k.startswith("$")]
-    # Separate out all PARAMETER commands; to be executed last
+    # Separate out all PARAMETER commands; to be handled last
     commands, para_commands = partition(
         lambda cmd: cmd.upper().startswith("PARA"),
         commands)
-    return newdb
+    # Separate out all FUNCTION commands
+    commands, func_commands = partition(
+        lambda cmd: cmd.upper().startswith("FUNC"),
+        commands)
+
+if __name__ == "__main__":
+    pass
