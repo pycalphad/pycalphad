1486a52e67637a1a4153b616f110302ee18db7f5
Richard Otis <richard.otis@outlook.com>
2014-10-15 09:51:57 -0400

    Reorganized TDB stuff into its own module

--- calphad/io/database.py
+++ calphad/io/database.py
@@ -3,6 +3,7 @@ associated with structured thermodynamic/kinetic data.
 """
 from sets import Set
 from tinydb import TinyDB
+import calphad.io.tdb
 
 class Database:
     """
@@ -219,52 +220,10 @@ class Database:
             raw_data = None
             with open(input_data, 'r') as data:
                 raw_data = data.read()
-            tdbread(self, raw_data)
+            calphad.io.tdb.tdbread(self, raw_data)
         else:
-            tdbread(self, input_data)
+            calphad.io.tdb.tdbread(self, input_data)
 
-from itertools import tee
-
-def partition(pred, iterable):
-    'Use a predicate to partition entries into false entries and true entries'
-    # partition(is_odd, range(10)) --> 0 2 4 6 8   and  1 3 5 7 9
-    tee1, tee2 = tee(iterable)
-    return [i for i in tee1 if not pred(i)], [i for i in tee2 if pred(i)]
-
-def tdbread(targetdb, lines):
-    """
-    Parse a TDB file into a pycalphad Database object.
-
-    Parameters
-    ----------
-    targetdb : Database
-        A pycalphad Database.
-    lines : string
-        A raw TDB file.
-    """
-    lines = lines.replace('\t', ' ')
-    lines = lines.strip()
-    # Split the string by newlines
-    splitlines = lines.split('\n')
-    # Remove extra whitespace inside line
-    splitlines = [' '.join(k.split()) for k in splitlines]
-    # Remove comments
-    splitlines = [k for k in splitlines if not k.startswith("$")]
-    # Combine everything back together
-    lines = ' '.join(splitlines)
-    # Now split by the command delimeter
-    commands = lines.split('!')
-    # Filter out comments one more time
-    # It's possible they were at the end of a command
-    commands = [k for k in commands if not k.startswith("$")]
-    # Separate out all PARAMETER commands; to be handled last
-    commands, para_commands = partition(
-        lambda cmd: cmd.upper().startswith("PARA"),
-        commands)
-    # Separate out all FUNCTION commands
-    commands, func_commands = partition(
-        lambda cmd: cmd.upper().startswith("FUNC"),
-        commands)
 
 if __name__ == "__main__":
     pass
