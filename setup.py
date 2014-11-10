c5958b52052feb29790e8ecfc8ea5d684c231c1b
Richard Otis <richard.otis@outlook.com>
2014-11-09 19:14:59 -0500

    Fix dependencies

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup
+import os
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='pycalphad',
     version='0.0.1',
     author='Richard Otis',
     author_email='richard.otis@outlook.com',
-    packages=['pycalphad','pycalphad.io', 'pycalphad.plot', 'pycalphad.plot.projections'],
-    license='',
-    install_requires=['numpy','matplotlib','pandas', 'sympy','pyparsing', 'tinydb'],
+    description='CALPHAD tools for designing thermodynamic models, calculating phase diagrams and investigating phase equilibria.',
+    packages=['pycalphad', 'pycalphad.io', 'pycalphad.plot', 'pycalphad.plot.projections'],
+    license='MIT',
+    long_description=read('README.md'),
+    url='https://github.com/richardotis/pycalphad',
+    install_requires=['numpy', 'matplotlib', 'pandas', 'sympy', 'pyparsing', 'tinydb', 'theano', 'scipy'],
     classifiers=['Development Status :: 3 - Alpha']
 )
