3b27ea334eb3e269939de5c2dd545ca341197a26
Richard Otis <richard.otis@outlook.com>
2015-04-09 16:39:57 -0400

    Single-source version support with Versioneer

--- pycalphad/__init__.py
+++ pycalphad/__init__.py
@@ -5,3 +5,6 @@ from pycalphad.eq.energy_surf import energy_surf
 from pycalphad.plot.isotherm import isotherm
 from pycalphad.plot.binary import binplot
 import pycalphad.variables as v
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -26,5 +26,29 @@ setup(
     long_description=read('README.md'),
     url='https://github.com/richardotis/pycalphad',
     install_requires=['matplotlib', 'pandas', 'sympy', 'pyparsing', 'tinydb', 'scipy', 'numpy'],
-    classifiers=['Development Status :: 3 - Alpha']
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ],
+
 )
