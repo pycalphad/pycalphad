7181294fef1ffac7f724feaee498fcdc0dcae3bb
Richard Otis <richard.otis@outlook.com>
2015-07-30 16:33:27 -0400

    BUG: Import ordering works when working directory is the same as the source directory

--- pycalphad/__init__.py
+++ pycalphad/__init__.py
@@ -1,3 +1,4 @@
+import pycalphad.variables as v
 from pycalphad.model import Model
 from pycalphad.io.database import Database
 from pycalphad.eq.calculate import calculate
@@ -5,7 +6,6 @@ from pycalphad.eq.equilibrium import equilibrium
 from pycalphad.eq.energy_surf import energy_surf
 from pycalphad.plot.isotherm import isotherm
 from pycalphad.plot.binary import binplot
-import pycalphad.variables as v
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
