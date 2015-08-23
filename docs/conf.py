dcca6f0059806f0e0b878797870c9d0b74372a97
Richard Otis <richard.otis@outlook.com>
2015-08-22 22:42:07 -0400

    BLD: enable sphinx mathjax extension [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ sys.path.insert(0, os.path.abspath('../pycalphad'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
