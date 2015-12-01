861bbaa5fd0427b9e8e2824bec01b1580a824815
Richard Otis <richard.otis@outlook.com>
2015-12-01 14:54:39 -0500

    WIP: fitting: plot_results can auto-compare specified TDBs, plus some minor tweaks for sublattice model harmonization

--- pycalphad/fitting.py
+++ pycalphad/fitting.py
@@ -4,8 +4,8 @@ This module contains routines for fitting new CALPHAD models.
 
 import numpy as np
 import xray
-from pycalphad import calculate, Model, Database
-from pycalphad.core.utils import make_callable
+from pycalphad import calculate, equilibrium, Model, Database
+from pycalphad.core.utils import make_callable, generate_dof
 import pycalphad.variables as v
 from sympy import Symbol
 import matplotlib.pyplot as plt
@@ -15,7 +15,7 @@ from collections import OrderedDict, namedtuple
 import json
 
 
-def setup_dataset(file_obj, dbf, params):
+def setup_dataset(file_obj, dbf, params, mode=None):
     # params should be a list of pymc variables corresponding to parameters
     data = json.load(file_obj)
     if data['solver']['mode'] != 'manual':
@@ -32,7 +32,7 @@ def setup_dataset(file_obj, dbf, params):
     site_fracs = {key: sorted(mod.ast.atoms(v.SiteFraction), key=str) for key, mod in fit_models.items()}
     # Call this from a routine that pulls in all datasets and generates the variance vars + Potentials
     callables = {name: make_callable(getattr(mod, data['output']),
-                                     itertools.chain(param_vars, indep_vars, site_fracs[name]))
+                                     itertools.chain(param_vars, indep_vars, site_fracs[name]), mode=mode)
                  for name, mod in fit_models.items()}
     extra_conds = OrderedDict({key: np.atleast_1d(value) for key, value in data['conditions'].items()})
     exp_values = xray.DataArray(np.array(data['values'], dtype=np.float),
@@ -116,23 +116,49 @@ def build_pymc_model(dbf, dataset_names, params):
     return mod, datasets
 
 
-def plot_results(datasets, params, databases=None):
+def _map_internal_dof(input_database, components, phase_name, points):
+    """
+    Map matrix of internal degrees of freedom to global compositions.
+    """
+    # Map the internal degrees of freedom to global coordinates
+    # Normalize site ratios by the sum of site ratios times a factor
+    # related to the site fraction of vacancies
+    phase_obj = input_database.phases[phase_name]
+    site_ratio_normalization = np.zeros(points.shape[:-1])
+    phase_compositions = np.empty(points.shape[:-1] + (len(components),))
+    variables, sublattice_dof = generate_dof(phase_obj, components)
+    for idx, sublattice in enumerate(phase_obj.constituents):
+        vacancy_column = np.ones(points.shape[:-1])
+        if 'VA' in set(sublattice):
+            var_idx = variables.index(v.SiteFraction(phase_obj.name, idx, 'VA'))
+            vacancy_column -= points[..., :, var_idx]
+        site_ratio_normalization += phase_obj.sublattices[idx] * vacancy_column
+
+    for col, comp in enumerate(components):
+        avector = [float(vxx.species == comp) *
+                   phase_obj.sublattices[vxx.sublattice_index] for vxx in variables]
+        phase_compositions[..., :, col] = np.divide(np.dot(points[..., :, :], avector),
+                                                    site_ratio_normalization)
+    return phase_compositions
+
+
+def plot_results(input_database, datasets, params, databases=None):
     """
     Generate figures using the datasets and trace of the parameters.
     A dict of label->Database objects may be provided as a kwarg.
     """
     # Add extra broadcast dimensions for T, P, and 'points'
-    param_tr = [i.trace()[None, None, None].T for i in params]
+    param_tr = [i[None, None, None].T for i in params.values()]
 
     def plot_key(obj):
         plot = obj.json.get('plot', None)
         return (plot['x'], plot['y']) if plot else None
-
     datasets = sorted(datasets, key=plot_key)
+    databases = dict() if databases is None else databases
     for plot_data_type, data_group in itertools.groupby(datasets, key=plot_key):
         if plot_data_type is None:
             continue
-        figure = plt.figure(figsize=(15,12))
+        figure = plt.figure(figsize=(15, 12))
         data_group = list(data_group)
         x, y = plot_data_type
         # All of data_group should be calculating the same thing...
@@ -147,6 +173,43 @@ def plot_results(datasets, params, databases=None):
         for data in data_group:
             plot_label = data.json['plot'].get('name', None)
             figure.gca().plot(data.exp_data[x].values, data.exp_data.values.flatten(), label=plot_label)
+        for label, dbf in databases.items():
+            # TODO: Relax this restriction
+            if data_group[0].json['solver']['mode'] != 'manual':
+                continue
+            conds = data_group[0].json['conditions']
+            conds['T'] = np.array(conds['T'])
+            conds['T'] = conds['T'][conds['T'] >= 300.]
+            for key in conds.keys():
+                if key not in ['T', 'P']:
+                    raise ValueError('Invalid conditions in JSON file')
+            # To work around differences in sublattice models, relax the internal dof
+            global_comps = sorted(set(data_group[0].json['components']) - set(['VA']))
+            compositions = \
+                _map_internal_dof(input_database,
+                                  sorted(data_group[0].json['components']),
+                                  data_group[0].json['phases'][0],
+                                  np.atleast_2d(
+                                      data_group[0].json['solver']['sublattice_configuration']).astype(np.float))
+            # Tiny perturbation to work around a bug in lower_convex_hull (gh-28)
+            compare_conds = {v.X(comp): np.add(compositions[:, idx], 1e-4).flatten().tolist()
+                             for idx, comp in enumerate(global_comps[:-1])}
+            compare_conds.update({v.__dict__[key]: value for key, value in conds.items()})
+            # We only want to relax the internal dof at the lowest temperature
+            # This will help us capture the most related sublattice config since solver mode=manual
+            # probably means this is first-principles data
+            compare_conds[v.T] = 300.
+            eqres = equilibrium(dbf, data_group[0].json['components'],
+                                str(data_group[0].json['phases'][0]), compare_conds, verbose=False)
+            internal_dof = sum(map(len, dbf.phases[data_group[0].json['phases'][0]].constituents))
+            largest_phase_fraction = eqres['NP'].values.argmax()
+            eqpoints = eqres['Y'].values[..., largest_phase_fraction, :internal_dof]
+            result = calculate(dbf, data_group[0].json['components'],
+                               str(data_group[0].json['phases'][0]), output=y,
+                               points=eqpoints, **conds)
+            # Don't show CALPHAD results below 300 K because they're meaningless right now
+            result = result.sel(T=slice(300, None))
+            figure.gca().plot(result[x].values.flatten(), result[y].values.flatten(), label=label)
         label_mapping = dict(x=x, y=y)
         label_mapping['CPM'] = 'Molar Heat Capacity (J/mol-atom-K)'
         label_mapping['SM'] = 'Molar Entropy (J/mol-atom-K)'
