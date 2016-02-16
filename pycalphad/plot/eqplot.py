b313ea9940891e62274e50de76a60cc907e90adb
Richard Otis <richard.otis@outlook.com>
2016-02-15 21:45:33 -0500

    ENH: binplot/eqplot: Add color back to ZPF lines. Nice performance improvement -- though equilibrium() is still a big bottleneck. Progress on gh-36.

--- pycalphad/plot/eqplot.py
+++ pycalphad/plot/eqplot.py
@@ -35,9 +35,50 @@ def _map_coord_to_variable(coord):
     else:
         return coord
 
+def eqplot(eq, ax=None, x=None, y=None, z=None, **kwargs):
+    """
+    Plot the result of an equilibrium calculation.
 
-def _binplot_setup(ax, phases, tie_lines, tie_line_colors, tie_line_widths):
-    "Setup the plot for a binary phase diagram."
+    Parameters
+    ----------
+    eq : xarray.Dataset
+        Result of equilibrium calculation.
+    ax : matplotlib.Axes
+        Default axes used if not specified.
+    x : StateVariable, optional
+    y : StateVariable, optional
+    z : StateVariable, optional
+    kwargs : kwargs
+        Passed to `matplotlib.pyplot.scatter`.
+
+    Returns
+    -------
+    matplotlib AxesSubplot
+    """
+    ax = plt.gca() if ax is None else ax
+    conds = OrderedDict([(_map_coord_to_variable(key), unpack_condition(np.asarray(value)))
+                         for key, value in sorted(eq.coords.items(), key=str)
+                         if (key == 'T') or (key == 'P') or (key.startswith('X_'))])
+    indep_comp = [key for key, value in conds.items() if isinstance(key, v.Composition) and len(value) > 1]
+    indep_pot = [key for key, value in conds.items() if ((key == v.T) or (key == v.P)) and len(value) > 1]
+    if (len(indep_comp) != 1) or (len(indep_pot) != 1):
+        raise ValueError('Plot currently requires exactly one composition and one potential coordinate')
+    indep_comp = indep_comp[0]
+    indep_pot = indep_pot[0]
+    x = indep_comp if x is None else x
+    y = indep_pot if y is None else y
+    if z is not None:
+        raise NotImplementedError('3D plotting is not yet implemented')
+    # TODO: Temporary workaround to fix string encoding issue when loading netcdf files from disk
+    phases = map(str, sorted(set(np.array(eq.Phase.values.ravel(), dtype='U')) - {''}, key=str))
+    comps = map(str, sorted(np.array(eq.coords['component'].values, dtype='U'), key=str))
+    eq['component'] = np.array(eq['component'], dtype='U')
+    eq['Phase'].values = np.array(eq['Phase'].values, dtype='U')
+
+    # Select all two-phase regions
+    two_phase_indices = np.nonzero(np.sum(eq.Phase.values != '', axis=-1, dtype=np.int) == 2)
+    found_phases = eq.Phase.values[two_phase_indices][..., :2]
+
     colorlist = {}
 
     # colors from Junwei Huang, March 21 2013
@@ -60,100 +101,38 @@ def _binplot_setup(ax, phases, tie_lines, tie_line_colors, tie_line_widths):
         colorlist[phase] = "#"+colorvalues[np.mod(phasecount, len(colorvalues))]
         legend_handles.append(mpatches.Patch(color=colorlist[phase], label=phase))
         phasecount += 1
-    ax.tick_params(axis='both', which='major', labelsize=14)
-    ax.grid(True)
-
-    # Get the configured plot colors
-    #plotcolors = list(map(lambda x: [colorlist[x[0]], colorlist[x[1]]],
-    #                      tie_lines[:, :, 2]))
-    if len(tie_lines) > 0:
-        lc = mc.LineCollection(
-            tie_lines, zorder=1, colors=tie_line_colors, linewidths=tie_line_widths
-        )
-        ax.add_collection(lc)
-        ax.scatter(tie_lines[:, :, 0].ravel(), tie_lines[:, :, 1].ravel(), s=3, zorder=2)
-
-
     # position the phase legend
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
     ax.legend(handles=legend_handles, loc='center left', bbox_to_anchor=(1, 0.5))
+    ax.tick_params(axis='both', which='major', labelsize=14)
+    ax.grid(True)
+    plotcolors = np.array(list(map(lambda x: [colorlist[x[0]], colorlist[x[1]]], found_phases)), dtype='U')
 
-    return ax
-
-
-def eqplot(eq, ax=None, x=None, y=None, z=None, **kwargs):
-    """
-    Plot the result of an equilibrium calculation.
-
-    Parameters
-    ----------
-    eq : xarray.Dataset
-        Result of equilibrium calculation.
-    ax : matplotlib.Axes
-        Default axes used if not specified.
-    x : StateVariable, optional
-    y : StateVariable, optional
-    z : StateVariable, optional
-    kwargs : kwargs
-        Passed to matplotlib.pyplot.plot()
-
-    Returns
-    -------
-    matplotlib Figure
-    """
-    ax = plt.gca() if ax is None else ax
-    tie_lines = []
-    tie_line_colors = []
-    tie_line_widths = []
-    phase_boundaries = OrderedDict()
-    conds = OrderedDict([(_map_coord_to_variable(key), unpack_condition(np.asarray(value)))
-                         for key, value in sorted(eq.coords.items(), key=str)
-                         if (key == 'T') or (key == 'P') or (key.startswith('X_'))])
-    indep_comp = [key for key, value in conds.items() if isinstance(key, v.Composition) and len(value) > 1]
-    indep_pot = [key for key, value in conds.items() if ((key == v.T) or (key == v.P)) and len(value) > 1]
-    if (len(indep_comp) != 1) or (len(indep_pot) != 1):
-        raise ValueError('Plot currently requires exactly one composition and one potential coordinate')
-    indep_comp = indep_comp[0]
-    indep_pot = indep_pot[0]
-    x = indep_comp if x is None else x
-    y = indep_pot if y is None else y
-    if z is not None:
-        raise NotImplementedError('3D plotting is not yet implemented')
-    # TODO: Temporary workaround to fix string encoding issue when loading netcdf files from disk
-    phases = map(str, sorted(set(np.array(eq.Phase.values.ravel(), dtype='U')) - {''}, key=str))
-    comps = map(str, sorted(np.array(eq.coords['component'].values, dtype='U'), key=str))
-    fixed_conds = {str(key): value[0] for key, value in conds.items() if len(value) == 1}
-    eq['component'] = np.array(eq['component'], dtype='U')
-    eq['Phase'].values = np.array(eq['Phase'].values, dtype='U')
-
-    full_eq_by_temp = eq.sel(**fixed_conds).groupby(str(indep_pot))
-    for current_temp, temp_slice in full_eq_by_temp:
-        full_eq_by_comp = temp_slice.groupby(str(indep_comp))
-        for current_comp, slice_eq in full_eq_by_comp:
-            # Select all two-phase regions in this composition
-            num_phases = np.sum((slice_eq.Phase != ''), dtype=np.int)
-            if num_phases != 2:
-                continue
-            first_vertex = slice_eq.sel(vertex=0, component=indep_comp.species)
-            first_tuple = (float(first_vertex[str(indep_pot)].values), float(first_vertex['X'].values), first_vertex['Phase'].values)
-            second_vertex = slice_eq.sel(vertex=1, component=indep_comp.species)
-            second_tuple = (float(second_vertex[str(indep_pot)].values), float(second_vertex['X'].values), second_vertex['Phase'].values)
-            tie_lines.append([[first_tuple[1], first_tuple[0]], [second_tuple[1], second_tuple[0]]])
-            # Green for a tie line (as is tradition)
-            tie_line_colors.append([0, 1, 0, 1])
-            tie_line_widths.append(0.5)
-    tie_lines = np.atleast_3d(tie_lines)
-    tie_line_colors = np.atleast_2d(tie_line_colors)
-    tie_line_widths = np.atleast_1d(tie_line_widths)
-
+    if isinstance(x, v.Composition):
+        compositions = eq.X.sel(component=x.species).values[two_phase_indices][..., :2]
+    else:
+        raise NotImplementedError('Plotting {} is not yet implemented'.format(x))
+    # Have to do some extra work to get all potential values for the given tie lines
+    temps = np.take(eq[str(y)].values, two_phase_indices[list(str(i) for i in conds.keys()).index(str(y))])
     if ax is None:
         ax = plt.gca()
-    ax = _binplot_setup(ax, phases, tie_lines, tie_line_colors, tie_line_widths)
+    # Draw zero phase-fraction lines
+    ax.scatter(compositions[..., 0], temps, s=3, c=plotcolors[..., 0], edgecolors='None', zorder=2, **kwargs)
+    ax.scatter(compositions[..., 1], temps, s=3, c=plotcolors[..., 1], edgecolors='None', zorder=2, **kwargs)
+    # Draw tie-lines
+    tielines = np.array([np.concatenate((compositions[..., 0][..., np.newaxis], temps[..., np.newaxis]), axis=-1),
+                         np.concatenate((compositions[..., 1][..., np.newaxis], temps[..., np.newaxis]), axis=-1)])
+    tielines = np.rollaxis(tielines, 1)
+    lc = mc.LineCollection(tielines, zorder=1, colors=[0, 1, 0, 1], linewidths=[0.5, 0.5])
+    ax.add_collection(lc)
     plot_title = '-'.join([x.title() for x in sorted(comps) if x != 'VA'])
     ax.set_title(plot_title, fontsize=20)
-    ax.set_xlim([np.min(conds[indep_comp])-1e-4, np.max(conds[indep_comp])+1e-4])
+    ax.set_xlim([np.min(conds[indep_comp])-1e-2, np.max(conds[indep_comp])+1e-2])
     ax.set_ylim([np.min(conds[indep_pot]), np.max(conds[indep_pot])])
-    ax.set_xlabel(indep_comp, labelpad=15, fontsize=20)
+    if isinstance(x, v.Composition):
+        ax.set_xlabel('X({})'.format(indep_comp.species), labelpad=15, fontsize=20)
+    else:
+        ax.set_xlabel(indep_comp, labelpad=15, fontsize=20)
     ax.set_ylabel(_plot_labels[indep_pot], fontsize=20)
     return ax
