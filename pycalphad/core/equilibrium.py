ENH: equilibrium/lower_convex_hull: Improve convergence and numerical stability of solver by fixing simplex selection in lower_convex_hull and adjusting the way Lagrange terms are handled in equilibrium. Also tighten numerical tolerance for miscibility gap detection.

pycalphad/core/equilibrium.py
@@ -194,9 +194,9 @@ def _compute_constraints(dbf, comps, phases, cur_conds, site_fracs, phase_fracs,
             active_in_subl = set(dbf.phases[name].constituents[idx]).intersection(comps)
             constraint_jac[constraint_offset + idx,
             var_idx:var_idx + len(active_in_subl)] = 1
-            # print('L_CONSTRAINTS[{}] = {}'.format(constraint_offset+idx, -(sum(site_fracs[var_idx:var_idx + len(active_in_subl)]) - 1)))
+            # print('L_CONSTRAINTS[{}] = {}'.format(constraint_offset+idx, (sum(site_fracs[var_idx:var_idx + len(active_in_subl)]) - 1)))
             l_constraints[constraint_offset + idx] = \
-                -(sum(site_fracs[var_idx:var_idx + len(active_in_subl)]) - 1)
+                (sum(site_fracs[var_idx:var_idx + len(active_in_subl)]) - 1)
             var_idx += len(active_in_subl)
         constraint_offset += len(dbf.phases[name].sublattices)
     # Second: Mass balance of each component
@@ -227,7 +227,7 @@ def _compute_constraints(dbf, comps, phases, cur_conds, site_fracs, phase_fracs,
             # TODO: Assuming N=1 (fixed for dependent component)
             l_constraints[constraint_offset] -= (1 - indep_sum)
             # print('L_CONSTRAINTS[{}] -= {}'.format(constraint_offset, (1-indep_sum)))
-        l_constraints[constraint_offset] *= -1
+        #l_constraints[constraint_offset] *= -1
         # print('L_CONSTRAINTS[{}] *= -1'.format(constraint_offset))
         constraint_offset += 1
     return l_constraints, constraint_jac, l_multipliers, chemical_potentials, mole_fractions
@@ -268,7 +268,7 @@ def _build_multiphase_gradient(dbf, comps, phases, cur_conds, site_fracs, phase_
         var_offset += phase_dof[phase_idx]
         phase_idx += 1
     gradient_term[:num_vars] += np.dot(constraint_jac.T, l_multipliers)
-    gradient_term[num_vars:] = l_constraints
+    gradient_term[num_vars:] = -l_constraints
     return gradient_term
 
 def _build_multiphase_system(dbf, comps, phases, cur_conds, site_fracs, phase_fracs,
@@ -313,7 +313,7 @@ def _build_multiphase_system(dbf, comps, phases, cur_conds, site_fracs, phase_fr
     l_hessian[num_vars:, :num_vars] = constraint_jac
     # print('PURE GRADIENT', gradient_term)
     gradient_term[:num_vars] += np.dot(constraint_jac.T, l_multipliers)
-    gradient_term[num_vars:] = l_constraints
+    gradient_term[num_vars:] = -l_constraints
     return l_hessian, gradient_term
 
 def _eqcalculate(dbf, comps, phases, conditions, output, data=None, per_phase=False, **kwargs):
@@ -975,7 +975,8 @@ def equilibrium(dbf, comps, phases, conditions, output=None, model=None,
             # Take the largest step which reduces the energy
             old_energy = copy.deepcopy(properties['GM'].values[it.multi_index])
             #print('OLD ENERGY', old_energy)
-            old_constrained_objective = old_energy + np.abs(l_multipliers * l_constraints).sum()
+            # XXX: Why is it necessary to square this?
+            old_constrained_objective = old_energy + ((l_multipliers * l_constraints).sum()) ** 2
             #print('OLD OBJ', old_constrained_objective)
             old_chem_pots = properties['MU'].values[it.multi_index].copy()
             #print('STARTING ALPHA', alpha)
@@ -1013,14 +1014,18 @@ def equilibrium(dbf, comps, phases, conditions, output=None, model=None,
                     _compute_constraints(dbf, comps, phases, cur_conds,
                                          candidate_site_fracs, candidate_phase_fracs, phase_records,
                                          l_multipliers=candidate_l_multipliers)
-                #print('CANDIDATE L MULTIPLIERS', candidate_l_multipliers)
+                #print('CANDIDATE L MULTIPLIERS AFTER', candidate_l_multipliers)
                 #print('CANDIDATE_L_CONSTRAINTS', candidate_l_constraints)
                 #print('CANDIDATE L MULS', candidate_l_constraints * candidate_l_multipliers)
+                #print('CANDIDATE L MUL SUM', (candidate_l_multipliers *
+                #                                    candidate_l_constraints).sum()
+                #      )
                 candidate_energy = _compute_multiphase_objective(dbf, comps, phases, cur_conds, candidate_site_fracs,
                                                                  candidate_phase_fracs,
                                                                  callable_dict)
-                candidate_constrained_objective = candidate_energy + np.abs(candidate_l_multipliers * \
-                                                                            candidate_l_constraints).sum()
+                # XXX: Why is it necessary to square this?
+                candidate_constrained_objective = candidate_energy + ((candidate_l_multipliers *
+                                                                       candidate_l_constraints).sum()) ** 2
                 #print('CANDIDATE CHEM POTS', candidate_chem_pots)
                 #print('CANDIDATE ENERGY', candidate_energy)
                 #print('CANDIDATE OBJ', candidate_constrained_objective)

pycalphad/core/lower_convex_hull.py
@@ -5,6 +5,7 @@ equilibrium calculation.
 from __future__ import print_function
 from pycalphad.log import logger
 from pycalphad.core.cartesian import cartesian
+from pycalphad.core.constants import MIN_SITE_FRACTION
 import numpy as np
 
 # The energetic difference, in J/mol-atom, below which is considered 'zero'
@@ -31,6 +32,23 @@ def _initialize_array(global_grid, result_array):
     # Note: We're assuming that the max energy is in the first few, presumably
     # fictitious points instead of more rigorously checking with argmax.
     result_array['points'].values[...] = np.arange(len_comps)
+
+def stacked_lstsq(L, b, rcond=1e-10):
+    """
+    Solve L x = b, via SVD least squares cutting of small singular values
+    L is an array of shape (..., M, N) and b of shape (..., M).
+    Returns x of shape (..., N)
+    This code is necessary until stacked arrays are supported in numpy.linalg.lstsq.
+    Source: http://stackoverflow.com/questions/30442377/how-to-solve-many-overdetermined-systems-of-linear-equations-using-vectorized-co
+    """
+    u, s, v = np.linalg.svd(L, full_matrices=False)
+    s_max = s.max(axis=-1, keepdims=True)
+    s_min = rcond*s_max
+    inv_s = np.zeros_like(s)
+    inv_s[s >= s_min] = 1/s[s>=s_min]
+    x = np.einsum('...ji,...j->...i', v,
+                  inv_s * np.einsum('...ji,...j->...i', u, b.conj()))
+    return np.conj(x, x)
 
 def lower_convex_hull(global_grid, result_array, verbose=False):
     """
@@ -106,6 +124,11 @@ def lower_convex_hull(global_grid, result_array, verbose=False):
                                       1 - np.sum(comp_values, keepdims=True, axis=-1),
                                       comp_values[..., insert_idx:]),
                                      axis=-1)
+        # Prevent compositions near an edge from going negative
+        comp_values[np.nonzero(comp_values < MIN_SITE_FRACTION)] = MIN_SITE_FRACTION*10
+        # TODO: Assumes N=1
+        comp_values /= comp_values.sum(axis=-1, keepdims=True)
+        #print(comp_values)
 
     # SECOND CASE: Only chemical potential conditions specified
     # TODO: Implementation of chemical potential
@@ -150,8 +173,9 @@ def lower_convex_hull(global_grid, result_array, verbose=False):
         # This is because even one degenerate simplex causes the entire tensor
         #     to be singular
         nondegenerate_indices = np.all(np.linalg.svd(trial_matrix,
-                                                     compute_uv=False) > 1e-12,
+                                                     compute_uv=False) > 1e-09,
                                        axis=-1, keepdims=True)
+        #('NONDEGENERATE INDICES', nondegenerate_indices)
         # Determine how many trial simplices remain for each target point.
         # In principle this would always be one simplex per point, but once
         # some target values reach equilibrium, trial_points starts
@@ -159,9 +183,10 @@ def lower_convex_hull(global_grid, result_array, verbose=False):
         # This causes trial_simplices to create degenerate simplices.
         # We can safely filter them out since those target values are
         # already at equilibrium.
-        sum_array = np.sum(nondegenerate_indices, axis=-1, dtype=np.int)
-        index_array = np.repeat(np.arange(trial_matrix.shape[0], dtype=np.int),
-                                sum_array)
+        #sum_array = np.sum(nondegenerate_indices, axis=-1, dtype=np.int)
+        #index_array = np.repeat(np.arange(trial_matrix.shape[0], dtype=np.int),
+        #                        sum_array)
+        index_array = np.arange(trial_matrix.shape[0], dtype=np.int)
         comp_shape = trial_simplices.shape[:len(indep_conds)+len(pot_conds)] + \
                      (comp_values.shape[0], trial_simplices.shape[-2])
 
@@ -169,30 +194,42 @@ def lower_convex_hull(global_grid, result_array, verbose=False):
         fractions = np.full(result_array['points'].values.shape + \
                             (result_array['points'].values.shape[-1],), -1.)
         fractions[np.unravel_index(index_array, fractions.shape[:-1])] = \
-            np.linalg.solve(np.swapaxes(trial_matrix[index_array], -2, -1),
-                            comp_values[comp_indices])
+            stacked_lstsq(np.swapaxes(trial_matrix[index_array], -2, -1),
+                          comp_values[comp_indices])
+        fractions /= fractions.sum(axis=-1, keepdims=True)
+        #print('fractions', fractions)
         # A simplex only contains a point if its barycentric coordinates
         # (phase fractions) are non-negative.
-        bounding_indices = np.all(fractions >= 0, axis=-1)
+        bounding_indices = np.all(fractions >= -MIN_SITE_FRACTION*100, axis=-1)
         #print('BOUNDING INDICES', bounding_indices)
-        #zero_success_trials = np.sum(bounding_indices, axis=-1, dtype=np.int, keepdims=False) == 0
-        #if np.any(zero_success_trials):
-        #    print(trial_matrix[np.nonzero(zero_success_trials)[:-1]])
-        # If more than one trial simplex satisfies the non-negativity criteria
-        # then just choose the first non-degenerate one. This addresses gh-28.
-        # There is also the possibility that *none* of the trials were successful.
-        # This is usually due to numerical problems at the limit of composition space.
-        # We will sidestep the issue here by forcing the last first non-degenerate simplex to match in that case.
+        if ~np.any(bounding_indices):
+            raise ValueError('Desired composition is not inside any candidate simplex. This is a bug.')
         multiple_success_trials = np.sum(bounding_indices, axis=-1, dtype=np.int, keepdims=False) != 1
         #print('MULTIPLE SUCCESS TRIALS SHAPE', np.nonzero(multiple_success_trials))
         if np.any(multiple_success_trials):
-            saved_trial = np.argmax(np.logical_or(bounding_indices[np.nonzero(multiple_success_trials)],
-                                                  nondegenerate_indices.reshape(bounding_indices.shape)[np.nonzero(multiple_success_trials)]), axis=-1)
-            #print('SAVED TRIAL', saved_trial)
+            saved_trial = np.zeros_like(multiple_success_trials, dtype=np.int)
+            # Case of only degenerate simplices (zero bounding)
+            # Choose trial with "least negative" fraction
+            zero_success_indices = np.logical_and(~nondegenerate_indices.reshape(bounding_indices.shape),
+                                                  multiple_success_trials[..., np.newaxis])
+            saved_trial[np.nonzero(zero_success_indices.any(axis=-1))] = \
+                np.argmax(fractions[np.nonzero(zero_success_indices.any(axis=-1))].min(axis=-1), axis=-1)
+            # Case of multiple bounding non-degenerate simplices
+            # Choose the first one. This addresses gh-28.
+            multiple_bounding_indices = \
+                np.logical_and(np.logical_and(bounding_indices, nondegenerate_indices.reshape(bounding_indices.shape)),
+                               multiple_success_trials[..., np.newaxis])
+            #print('MULTIPLE SUCCESS TRIALS.shape', multiple_success_trials.shape)
+            #print('BOUNDING INDICES.shape', bounding_indices.shape)
+            #print('MULTIPLE_BOUNDING_INDICES.shape', multiple_bounding_indices.shape)
+            saved_trial[np.nonzero(multiple_bounding_indices.any(axis=-1))] = \
+                np.argmax(multiple_bounding_indices[np.nonzero(multiple_bounding_indices.any(axis=-1))], axis=-1)
+            #print('SAVED TRIAL.shape', saved_trial.shape)
             #print('BOUNDING INDICES BEFORE', bounding_indices)
             bounding_indices[np.nonzero(multiple_success_trials)] = False
             #print('BOUNDING INDICES FALSE', bounding_indices)
-            bounding_indices[np.nonzero(multiple_success_trials) + np.index_exp[saved_trial]] = True
+            bounding_indices[np.nonzero(multiple_success_trials) + \
+                             (saved_trial[np.nonzero(multiple_success_trials)],)] = True
             #print('BOUNDING INDICES AFTER', bounding_indices)
         fractions.shape = (-1, fractions.shape[-1])
         bounding_indices.shape = (-1,)
@@ -214,8 +251,8 @@ def lower_convex_hull(global_grid, result_array, verbose=False):
         aligned_compositions = global_grid.X.values[np.index_exp[statevar_indices + (candidate_simplices,)]]
         #print('aligned_compositions', aligned_compositions)
         #print('aligned_energies', aligned_energies)
-        candidate_potentials = np.linalg.solve(aligned_compositions.astype(np.float, copy=False),
-                                               aligned_energies.astype(np.float, copy=False))
+        candidate_potentials = stacked_lstsq(aligned_compositions.astype(np.float, copy=False),
+                                             aligned_energies.astype(np.float, copy=False))
         #print('candidate_potentials', candidate_potentials)
         logger.debug('candidate_simplices: %s', candidate_simplices)
         comp_indices = np.unravel_index(index_array, comp_shape)[len(indep_conds)+len(pot_conds)]

pycalphad/tests/test_equilibrium.py
@@ -9,11 +9,12 @@ from numpy.testing import assert_allclose
 import numpy as np
 from pycalphad import Database, calculate, equilibrium, EquilibriumError, ConditionError
 import pycalphad.variables as v
-from pycalphad.tests.datasets import ALNIPT_TDB, ROSE_TDB, ALFE_TDB, ALNIFCC4SL_TDB
+from pycalphad.tests.datasets import ALNIPT_TDB, ALCOCRNI_TDB, ROSE_TDB, ALFE_TDB, ALNIFCC4SL_TDB
 
 ROSE_DBF = Database(ROSE_TDB)
 ALFE_DBF = Database(ALFE_TDB)
 ALNIFCC4SL_DBF = Database(ALNIFCC4SL_TDB)
+ALCOCRNI_DBF = Database(ALCOCRNI_TDB)
 
 # ROSE DIAGRAM TESTS
 # This will fail until the equilibrium engine is switched from Newton-Raphson
@@ -170,3 +171,36 @@ def test_eq_missing_component():
     equilibrium(ALNIFCC4SL_DBF, ['AL', 'NI', 'VA'], ['LIQUID'],
                 {v.T: 1523, v.X('AL'): 0.88811111111111107,
                  v.X('CO'): 0.11188888888888888, v.P: 101325}, pbar=False)
+
+def test_eq_ternary_edge_case_mass():
+    """
+    Equilibrium along an edge of composition space will still balance mass.
+    """
+    eq = equilibrium(ALCOCRNI_DBF, ['AL', 'CO', 'CR', 'VA'], ['L12_FCC', 'BCC_B2', 'LIQUID'],
+                     {v.T: 1523, v.X('AL'): 0.88811111111111107,
+                      v.X('CO'): 0.11188888888888888, v.P: 101325}, pbar=False)
+    mass_error = np.nansum(np.squeeze(eq.NP * eq.X), axis=-2) - \
+                 [0.88811111111111107, 0.11188888888888888, 0]
+    assert np.all(np.abs(mass_error) < 0.01)
+
+def test_eq_ternary_inside_mass():
+    """
+    Equilibrium in interior of composition space will still balance mass.
+    """
+    eq = equilibrium(ALCOCRNI_DBF, ['AL', 'CO', 'CR', 'VA'], ['L12_FCC', 'BCC_B2', 'LIQUID'],
+                     {v.T: 1523, v.X('AL'): 0.44455555555555554,
+                      v.X('CO'): 0.22277777777777777, v.P: 101325}, pbar=False)
+    mass_error = np.nansum(np.squeeze(eq.NP * eq.X), axis=-2) - \
+                 [0.44455555555555554, 0.22277777777777777, 0.333]
+    assert np.all(np.abs(mass_error) < 0.01)
+
+def test_eq_ternary_edge_misc_gap():
+    """
+    Equilibrium at edge of miscibility gap will still balance mass.
+    """
+    eq = equilibrium(ALCOCRNI_DBF, ['AL', 'CO', 'CR', 'VA'], ['L12_FCC', 'BCC_B2', 'LIQUID'],
+                     {v.T: 1523, v.X('AL'): 0.33366666666666667,
+                      v.X('CO'): 0.44455555555555554, v.P: 101325}, pbar=False)
+    mass_error = np.nansum(np.squeeze(eq.NP * eq.X), axis=-2) - \
+                 [0.33366666666666667, 0.44455555555555554, 0.22177777777777785]
+    assert np.all(np.abs(mass_error) < 0.001)