d6830c7318d0b03bf8f876ad30b775d89218542a
Richard Otis <richard.otis@outlook.com>
2016-08-07 18:07:01 -0400

    WIP: deploy.sh: Don't attempt to initialize an orphan gh-pages branch. This has been done already

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -35,10 +35,9 @@ SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy)
 git clone $REPO out
 cd out
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+git checkout $TARGET_BRANCH
 cd ..
 
 # Clean out any existing contents
