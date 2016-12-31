42e64ae73af5f2c7c6708b458a85b6c07f093878
Richard Otis <richard.otis@outlook.com>
2016-12-31 08:53:55 -0800

    BLD/WIP: Dockerization of dependencies on Travis CI

---
+++ ci/travis_legacy_deps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+wget -nc http://repo.continuum.io/miniconda/Miniconda2-3.19.0-MacOSX-x86_64.sh -O miniconda.sh;
+else
+wget -nc http://repo.continuum.io/miniconda/Miniconda2-3.19.0-Linux-x86_64.sh -O miniconda.sh;
+fi
+chmod +x miniconda.sh
+./miniconda.sh -b -f
+export PATH=$HOME/miniconda2/bin:$PATH
+conda update --yes conda
+conda create --yes -n condaenv python=$TRAVIS_PYTHON_VERSION
+conda install --yes -n condaenv pip
+source activate condaenv
+conda config --add channels conda-forge
