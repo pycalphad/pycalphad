281803659fde78c9612d727375425c95e575836f
Richard Otis <richard.otis@outlook.com>
2017-04-01 11:30:57 -0700

    BLD/TST: deploy.sh: Make .ssh directory before trying to write to known_hosts

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -73,7 +73,8 @@ openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../ci/deploy_key.enc
 chmod 600 ../ci/deploy_key
 eval `ssh-agent -s`
 ssh-add ../ci/deploy_key
-ssh-keyscan github.com > ~/.ssh/known_hosts
+mkdir ~/.ssh
+ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
