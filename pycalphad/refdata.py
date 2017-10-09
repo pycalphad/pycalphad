ffda2d414c7701740609d6cbe0caa09974f4a385
bocklund <brandonbocklund@gmail.com>
2017-10-09 16:50:32 -0400

    MAINT: Update version of ESPEI in refdata import error

--- pycalphad/refdata.py
+++ pycalphad/refdata.py
@@ -1,1 +1,1 @@
-raise ImportError('pycalphad.refdata has been moved to ESPEI. Please install ESPEI 0.2.2 or later.')
+raise ImportError('pycalphad.refdata has been moved to ESPEI. Please install ESPEI 0.3.1 or later.')
