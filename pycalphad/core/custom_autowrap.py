f48c2c94ca9864ff7e7f39cbb367fcc5b80c8afe
Richard Otis <richard.otis@outlook.com>
2018-02-04 08:57:26 -0800

    FIX: ThreadSafeCythonCodeWrapper: Force use of MinGW compiler on Windows. Linux seems to ignore the directive, so no platform checks are necessary.

--- pycalphad/core/custom_autowrap.py
+++ pycalphad/core/custom_autowrap.py
@@ -439,6 +439,11 @@ class ThreadSafeCythonCodeWrapper(CythonCodeWrapper):
         "{np_includes}"
         "        )")
 
+    setup_cfg = (
+        "[build_ext]\n"
+        "compiler = mingw32\n"
+    )
+
     pyx_imports = (
         "import numpy as np\n"
         "cimport numpy as np\n"
@@ -495,6 +500,8 @@ class ThreadSafeCythonCodeWrapper(CythonCodeWrapper):
             f.write(self.setup_template.format(ext_args=", ".join(ext_args),
                                                np_import=np_import,
                                                np_includes=np_includes))
+        with open(os.path.join(self.filepath, 'setup.cfg'), 'w') as f:
+            f.write(self.setup_cfg)
 
     def _process_files(self, routine):
         command = self.command
