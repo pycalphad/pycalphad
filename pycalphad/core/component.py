0cc6a2db204e46f59579ead40373a9cbaca19151
Richard Otis <richard.otis@jpl.nasa.gov>
2018-02-23 15:48:21 -0800

    MAINT: Remove unused prototype Component class

--- pycalphad/core/component.py
+++
@@ -1,9 +0,0 @@
-class Component(object):
-    # name, mass, charge
-    def __new__(cls, obj):
-        if isinstance(obj, Component):
-            return obj
-        retobj = super(Component, cls).__new__(cls, obj)
-        # Species
-        # string
-        return retobj
