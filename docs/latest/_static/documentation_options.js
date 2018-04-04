8bcd03af05e7f81358317d1803c7eabcc5542a84
Travis CI <ci@pycalphad.org>
2018-04-04 15:26:37 +0000

    DOC: Deploy 'latest' docs to website: 269435337d15442ca57204c1a5b457661f74184a

--- docs/latest/_static/documentation_options.js
+++ docs/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0.7+2.gf632a77',
+    VERSION: '0.7+3.g2694353',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
