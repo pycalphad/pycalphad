e34a832bb03cdfb596aa606b4df82a00eb4eb169
Richard Otis <richard.otis@outlook.com>
2018-08-17 10:29:04 -0700

    FIX: SymPy 1.2 compatibility (#180)
    Provides compatibility fixes for codegen changes, as well as a workaround for sympy/sympy#14933

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     license='MIT',
     long_description=read('README.rst'),
     url='https://pycalphad.org/',
-    install_requires=['matplotlib', 'pandas', 'xarray!=0.8', 'sympy>=1.0', 'pyparsing', 'Cython>=0.24',
+    install_requires=['matplotlib', 'pandas', 'xarray!=0.8', 'sympy>=1.2', 'pyparsing', 'Cython>=0.24',
                       'tinydb', 'scipy', 'numpy>=1.9', 'dask[complete]>=0.15', 'dill', 'ipopt'],
     classifiers=[
         # How mature is this project? Common values are
