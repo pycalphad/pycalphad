f694a14727df64dd0a3f187d1fe750173d04b30a
Travis CI <ci@pycalphad.org>
2018-08-17 21:47:39 +0000

    DOC: Deploy 'latest' docs to website: 916ad73b256c1fa1a5a4306d754fc9e18798ab1c

--- docs/latest/_static/documentation_options.js
+++ docs/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.7+9.ge34a832',
+    VERSION: '0.7+10.g916ad73',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
