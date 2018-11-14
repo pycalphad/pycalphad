5d3fb3ed3f0c8ded54731c2d48ec6bb3fb0923b3
Travis CI <ci@pycalphad.org>
2018-11-14 15:17:06 +0000

    DOC: Deploy 'latest' docs to website: 5535526f10a8b8b7c81dc72af03de021822fe7ad

--- docs/latest/_static/documentation_options.js
+++ docs/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.7+16.g0a570d4',
+    VERSION: '0.7+17.g5535526',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
