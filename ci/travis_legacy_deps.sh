13d8c1e1c9cbb57794682023fbe601380d88f172
Richard Otis <richard.otis@jpl.nasa.gov>
2019-01-31 08:31:05 -0800

    TST/FIX: New way of pulling conda into PATH

--- ci/travis_legacy_deps.sh
+++ ci/travis_legacy_deps.sh
@@ -6,6 +6,7 @@ wget -nc http://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O
 fi
 chmod +x miniconda.sh
 ./miniconda.sh -b -f
+. $HOME/miniconda2/etc/profile.d/conda.sh
 conda update --yes conda
 conda create --yes -n condaenv python=$TRAVIS_PYTHON_VERSION
 conda install --yes -n condaenv pip
