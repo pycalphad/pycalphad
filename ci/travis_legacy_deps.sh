da1e380abc0f72f8f8b627e553f980997ec8c037
Richard Otis <richard.otis@jpl.nasa.gov>
2019-01-31 08:25:22 -0800

    TST: Switch to modern conda environment management

--- ci/travis_legacy_deps.sh
+++ ci/travis_legacy_deps.sh
@@ -6,7 +6,6 @@ wget -nc http://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O
 fi
 chmod +x miniconda.sh
 ./miniconda.sh -b -f
-export PATH=$HOME/miniconda2/bin:$PATH
 conda update --yes conda
 conda create --yes -n condaenv python=$TRAVIS_PYTHON_VERSION
 conda install --yes -n condaenv pip
