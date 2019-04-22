bd498f6fad3b2b41af14beb571d49a54d4017517
bocklund <brandonbocklund@gmail.com>
2019-04-22 13:25:57 -0400

    Bump sympy to 1.4

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     license='MIT',
     long_description=read('README.rst'),
     url='https://pycalphad.org/',
-    install_requires=['matplotlib', 'pandas', 'xarray!=0.8', 'sympy>=1.2', 'pyparsing', 'Cython>=0.24',
+    install_requires=['matplotlib', 'pandas', 'xarray!=0.8', 'sympy>=1.4', 'pyparsing', 'Cython>=0.24',
                       'tinydb', 'scipy', 'numpy>=1.13', 'dask[complete]>=0.18', 'dill', 'ipopt'],
     classifiers=[
         # How mature is this project? Common values are
