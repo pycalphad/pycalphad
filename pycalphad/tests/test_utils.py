5959be6439f4a0d2e0522c41f01986f761340da5
Brandon Bocklund <brandonbocklund@gmail.com>
2019-05-22 13:01:54 -0500

    FIX: Improve Model instantiation (#217)
    Two changes here:
    
    1. `instantiate_models` now returns a dict instead of a default dict to be more explicit. Only phases passed to `instantiate_models` are included.
    2. Fixed a subtle bug in calculate where over-specified models could have led to inconsistent maximum internal degree of freedom between `calculate` and `starting_point` because `calculate` based the internal dof size on all the model instances, while starting_point used the phase records (active phases only).

--- pycalphad/tests/test_utils.py
+++ pycalphad/tests/test_utils.py
@@ -2,8 +2,8 @@
 The utils test module contains tests for pycalphad utilities.
 """
 
-from pycalphad import Database
-from pycalphad.core.utils import filter_phases, unpack_components
+from pycalphad import Database, Model
+from pycalphad.core.utils import filter_phases, unpack_components, instantiate_models
 
 from pycalphad.tests.datasets import ALNIPT_TDB
 
@@ -15,9 +15,27 @@ def test_filter_phases_removes_disordered_phases_from_order_disorder():
     filtered_phases = set(filter_phases(ALNIPT_DBF, unpack_components(ALNIPT_DBF, ['AL', 'NI', 'PT', 'VA'])))
     assert all_phases.difference(filtered_phases) == {'FCC_A1'}
 
+
 def test_filter_phases_removes_phases_with_inactive_sublattices():
     """Phases that have no active components in any sublattice should be filtered"""
     all_phases = set(ALNIPT_DBF.phases.keys())
     filtered_phases = set(filter_phases(ALNIPT_DBF, unpack_components(ALNIPT_DBF, ['AL', 'NI', 'VA'])))
     assert all_phases.difference(filtered_phases) == {'FCC_A1', 'PT8AL21', 'PT5AL21', 'PT2AL', 'PT2AL3', 'PT5AL3', 'ALPT2'}
 
+
+def test_instantiate_models_only_returns_desired_phases():
+    """instantiate_models should only return phases passed"""
+    comps = ['AL', 'NI', 'VA']
+    phases = ['FCC_A1', 'LIQUID']
+
+    # models are overspecified w.r.t. phases
+    too_many_phases = ['FCC_A1', 'LIQUID', 'AL3NI1']
+    too_many_models = {phase: Model(ALNIPT_DBF, comps, phase) for phase in too_many_phases}
+    inst_mods = instantiate_models(ALNIPT_DBF, comps, phases, model=too_many_models)
+    assert len(inst_mods) == len(phases)
+
+    # models are underspecified w.r.t. phases
+    too_few_phases = ['FCC_A1']
+    too_few_models = {phase: Model(ALNIPT_DBF, comps, phase) for phase in too_few_phases}
+    inst_mods = instantiate_models(ALNIPT_DBF, comps, phases, model=too_few_models)
+    assert len(inst_mods) == len(phases)
