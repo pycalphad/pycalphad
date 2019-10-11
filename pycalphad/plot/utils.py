3c9a1071c88473f379604e7726bdbe1057f375f3
bocklund <brandonbocklund@gmail.com>
2019-10-11 15:40:38 -0400

    Reformat to uppercase

--- pycalphad/plot/utils.py
+++ pycalphad/plot/utils.py
@@ -28,13 +28,13 @@ def phase_legend(phases):
     # colors from HU1SUN, August 5 2018, ordered by igorjrd, issue #97
     # exclude green and red because of their special meaning on the diagram
     colorvalues = [
-        '00538a', 'f4c800', 'f13a13', 'c10020', 'd2f300', '53377a', '7bd1ec',
-        '232c16', 'fe4262', 'c0de00', '704aa4', 'ffb300', '176136', '7f180d',
-        '93aa00', '2b85eb', 'f6768e', '007d34', '803e75', '4a7c01', 'ff8e00',
-        'ec1840', '178d39', 'b32851', '577c23', 'a6bdd7', 'fd522b', '526b2e',
-        '90324e', '593315', 'b6a0d4', 'ff6800', 'cea262', '9a7cc4', '91250b',
-        '7b3d0b', 'ff7a5c', 'c01f3d', 'd39336', '817066', '96541f', 'eb9867',
-        'b15106', 'ee8548', '97691c', 'df6b10', '987155', 'c76f32', 'b37347'
+        '00538A', 'F4C800', 'F13A13', 'C10020', 'D2F300', '53377A', '7BD1EC',
+        '232C16', 'FE4262', 'C0DE00', '704AA4', 'FFB300', '176136', '7F180D',
+        '93AA00', '2B85EB', 'F6768E', '007D34', '803E75', '4A7C01', 'FF8E00',
+        'EC1840', '178D39', 'B32851', '577C23', 'A6BDD7', 'FD522B', '526B2E',
+        '90324E', '593315', 'B6A0D4', 'FF6800', 'CEA262', '9A7CC4', '91250B',
+        '7B3D0B', 'FF7A5C', 'C01F3D', 'D39336', '817066', '96541F', 'EB9867',
+        'B15106', 'EE8548', '97691C', 'DF6B10', '987155', 'C76F32', 'B37347',
     ]
     mxx = len(colorvalues)
     phasecount = 0
