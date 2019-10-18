166646db403a939864589e8ba1eacfbb7447966b
Igor J Marques <52574978+igorjrd@users.noreply.github.com>
2019-10-18 13:08:11 -0300

    Update test_utils.py

--- pycalphad/tests/test_utils.py
+++ pycalphad/tests/test_utils.py
@@ -11,12 +11,11 @@ ALNIPT_DBF = Database(ALNIPT_TDB)
 
 def test_filter_phases_removes_disordered_phases_from_order_disorder():
     """Databases with order-disorder models should have the disordered phases be filtered."""
-#%%
-all_phases = set(ALNIPT_DBF.phases.keys())
-filtered_phases = set(filter_phases(ALNIPT_DBF, unpack_components(ALNIPT_DBF, ['AL', 'NI', 'PT', 'VA'])))
-active_phases = check_order_disorder(ALNIPT_DBF, filtered_phases)
-assert all_phases.difference(active_phases) == {'FCC_A1'}
-#%%
+    all_phases = set(ALNIPT_DBF.phases.keys())
+    filtered_phases = set(filter_phases(ALNIPT_DBF, unpack_components(ALNIPT_DBF, ['AL', 'NI', 'PT', 'VA'])))
+    active_phases = check_order_disorder(ALNIPT_DBF, filtered_phases)
+    assert all_phases.difference(active_phases) == {'FCC_A1'}
+
 
 def test_filter_phases_removes_phases_with_inactive_sublattices():
     """Phases that have no active components in any sublattice should be filtered"""
