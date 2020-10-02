ad9384feddde87b7813caefeea7b0d220c5d0518
Richard Otis <rotis@jpl.nasa.gov>
2020-10-02 12:06:25 -0700

    BLD: Bump symengine to 0.6.1

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     long_description=read('README.rst'),
     url='https://pycalphad.org/',
     install_requires=['matplotlib', 'pandas', 'xarray>=0.11.2', 'sympy==1.5.1', 'pyparsing', 'Cython>=0.24',
-                      'tinydb>=3.8', 'scipy', 'numpy>=1.13', 'ipopt', 'symengine==0.6'],
+                      'tinydb>=3.8', 'scipy', 'numpy>=1.13', 'ipopt', 'symengine==0.6.1'],
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
