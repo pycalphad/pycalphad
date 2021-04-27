pycalphad/pycalphad

commit 50389503f126ea6f9e40947c215bd857bcfbd9de

FIX: Improve strictness when parsing TDB FUNCTION and PARAMETER lines (#308)
This PR makes the TDB parser more strict by requiring that PARAMETER and FUNCTION lines are terminated by an optional-suppressed reference key and a line end. This fixes behavior where the parser would silently consume all TDB content until the next line terminator (`!`) for these two commands once the `func_expr` grammar was successfully parsed.

- `FUNCTION` and `PARAMETER` pyparsing grammars are updated to include through the line ending, if the parsed function expression and reference key is not following by a line ending, a `ParseException` is now raised.
- A failing test based on #307 was created and merging this will close #307
- Before this change, I had 522 databases that were known to be parsed successfully pycalphad. After making the change, all 522 still parsed successfully. The only TDB that did not parse correctly was the ALNIPT_TDB database in our test suite, which had references of `REF: 0`, which I changed to `REF0` (I also verified that Thermo-Calc 2020b says that `REF: 0` is an invalid reference format in GES6)

pycalphad/io/tdb.py
@@ -216,7 +216,8 @@ def _tdb_grammar(): #pylint: disable=R0914
         Suppress(White()) + CharsNotIn(' !', exact=1) + SkipTo(LineEnd())
     # FUNCTION
     cmd_function = TCCommand('FUNCTION') + symbol_name + \
-        func_expr.setParseAction(_make_piecewise_ast)
+        func_expr.setParseAction(_make_piecewise_ast) + \
+        Optional(Suppress(Word(alphanums)('reference_key'))) + LineEnd()
     # ASSESSED_SYSTEMS
     cmd_ass_sys = TCCommand('ASSESSED_SYSTEMS') + SkipTo(LineEnd())
     # DEFINE_SYSTEM_DEFAULT
@@ -250,7 +251,8 @@ def _tdb_grammar(): #pylint: disable=R0914
         Optional(Suppress('&') + Word(alphas+'/-', min=1, max=2), default=None) + \
         Suppress(',') + constituent_array + \
         Optional(Suppress(';') + int_number, default=0) + \
-        Suppress(')') + func_expr.setParseAction(_make_piecewise_ast)
+        Suppress(')') + func_expr.setParseAction(_make_piecewise_ast) + \
+        Optional(Suppress(Word(alphanums)('reference_key'))) + LineEnd()
     # Now combine the grammar together
     all_commands = cmd_element | \
                     cmd_species | \

pycalphad/tests/datasets.py
@@ -579,7 +579,7 @@ $*********************  FCC_A1
    PARAMETER G(FCC_A1,PT:VA;0)  2.98150E+02  +GHSERPT#;  4.00000E+03  N
   REF283 !
 $   PARAMETER TC(FCC_A1,PT:VA;0)  2.98150E+02  -307.85;   6.00000E+03
-$   N REF: 0 !
+$   N REF0 !
    PARAMETER TC(FCC_A1,NI:VA;0)  2.98150E+02  633;   6.00000E+03   N 89DIN !
    PARAMETER BMAGN(FCC_A1,NI:VA;0)  2.98150E+02  .52;   6.00000E+03 N 89DIN !
 $  PARAMETER L(fcc_a1,al,pt:va;0) 2.98150E+02 -262446.89; 2.90000E+03 N !
@@ -1147,7 +1147,7 @@ $******************************** Compounds in AL-Pt
 $  PHASE AL21PT8 % 2 .724138 .275862 !
 $      CONS AL21PT8 :AL:PT NI:!
 $  PARAMETER G(AL21PT8,AL:NI;0)  2.98150E+02 -35000+.724138*GHSERAL#
-$    +.275862*GHSERNI#;   6.00000E+03   N REF: 0 !
+$    +.275862*GHSERNI#;   6.00000E+03   N REF0 !
 
 $   PARAMETER G(AL21PT8,AL:PT;0)  298.15  -82342+23.7*T+.7242*GHSERAL#
 $  +.2759*GHSERPT#;   6000   N REF0 !
@@ -1155,9 +1155,9 @@ $  +.2759*GHSERPT#;   6000   N REF0 !
   PHASE ALPT % 2 .5 .5 !
   CONS ALPT :AL:PT NI:!
   PARAMETER G(ALPT,AL:NI;0)  2.98150E+02 VA70+VA71*T+.5*GHSERAL#+.5*GHSERNI#;
-     6.00000E+03   N REF: 0 !
+     6.00000E+03   N REF0 !
   PARAMETER G(ALPT,AL:NI,PT;0)  2.98150E+02  VA72+VA73*T;   6.00000E+03
-     N REF: 0 !
+     N REF0 !
    PARAMETER G(ALPT,AL:PT;0)  298.15  +.5*GHSERAL#+.5*GHSERPT#+VX85#
   +VX95#*T;   6000.0   N REF0 !
 $*******************

pycalphad/tests/test_database.py
@@ -662,3 +662,19 @@ def test_database_applies_late_type_def():
     assert dbf.phases['BCC_A2'].model_hints['ihj_magnetic_afm_factor'] == -1.0
     assert 'ihj_magnetic_structure_factor' in dbf.phases['BCC_A2'].model_hints
     assert dbf.phases['BCC_A2'].model_hints['ihj_magnetic_structure_factor'] == 0.4
+
+
+def test_tdb_parser_raises_unterminated_parameters():
+    """A TDB FUNCTION or PARAMETER should give an error if the parsed
+    Piecewise expression does not end the line."""
+    # The PARAMETER G(BCC,FE:H;0) parameter is not terminated by an `!`.
+    # The parser merges all newlines until the `!`, meaning both parameters
+    # will be joined on one "line". The parser should raise an error.
+    UNTERMINATED_PARAM_STR = """     PARAMETER G(BCC,FE:H;0) 298.15  +GHSERFE+1.5*GHSERHH
+        +258000-3170*T+498*T*LN(T)-0.275*T**2; 1811.00  Y
+        +232264+82*T+1*GHSERFE+1.5*GHSERHH; 6000.00  N
+
+     PARAMETER G(BCC,FE:VA;0)      298.15 +GHSERFE; 6000 N ZIM !
+    """
+    with pytest.raises(ParseException):
+        Database(UNTERMINATED_PARAM_STR)

commit cc9c5bb635c215f3fb0227630ce1a3b294d7f5c5

FIX: add more TDB reference key characters (#326)
This PR fixes a regression introduced in #308 where the reference key in TDB `FUNCTION` and `PARAMETER` lines was limited to alphanumeric characters. Now `:_-` characters as well to fix some local databases that fail to parse. It's unclear what other valid characters could be. Thermo-Calc gives examples in the documentation that include `REF:250`, `REF_002`, and `REF-SGTE`, so at least those are confirmed valid characters.

On my on the 522 local TDB files, this dropped the parse failures from 41 to 14. The 14 failures to parse are due to whitespace in the reference key, e.g. `PARAM ...; 6000 N REF: 0 !`, which is invalid per Thermo-Calc's database checker.

pycalphad/io/tdb.py
@@ -197,6 +197,7 @@ def _tdb_grammar(): #pylint: disable=R0914
     ref_phase_name = symbol_name = Word(alphanums+'_-:()/', min=1)
     # species name, e.g., CO2, AL, FE3+
     species_name = Word(alphanums+'+-*/_.', min=1) + Optional(Suppress('%'))
+    reference_key = Word(alphanums+':_-')('reference_key')
     # constituent arrays are colon-delimited
     # each subarray can be comma- or space-delimited
     constituent_array = Group(delimitedList(Group(OneOrMore(Optional(Suppress(',')) + species_name)), ':'))
@@ -217,7 +218,7 @@ def _tdb_grammar(): #pylint: disable=R0914
     # FUNCTION
     cmd_function = TCCommand('FUNCTION') + symbol_name + \
         func_expr.setParseAction(_make_piecewise_ast) + \
-        Optional(Suppress(Word(alphanums)('reference_key'))) + LineEnd()
+        Optional(Suppress(reference_key)) + LineEnd()
     # ASSESSED_SYSTEMS
     cmd_ass_sys = TCCommand('ASSESSED_SYSTEMS') + SkipTo(LineEnd())
     # DEFINE_SYSTEM_DEFAULT
@@ -252,7 +253,7 @@ def _tdb_grammar(): #pylint: disable=R0914
         Suppress(',') + constituent_array + \
         Optional(Suppress(';') + int_number, default=0) + \
         Suppress(')') + func_expr.setParseAction(_make_piecewise_ast) + \
-        Optional(Suppress(Word(alphanums)('reference_key'))) + LineEnd()
+        Optional(Suppress(reference_key)) + LineEnd()
     # Now combine the grammar together
     all_commands = cmd_element | \
                     cmd_species | \