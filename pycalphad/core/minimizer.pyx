aaae7efa1783e46d7b69247d44c75a2271a28792
Richard Otis <rotis@jpl.nasa.gov>
2021-05-21 14:39:32 -0700

    MAINT: minimizer: Remove commented code

--- pycalphad/core/minimizer.pyx
+++ pycalphad/core/minimizer.pyx
@@ -833,7 +833,6 @@ cpdef find_solution(list compsets, int[::1] free_stable_compset_indices,
         # Phases that "want" to be removed will keep having their phase_amt set to zero, so mass balance is unaffected
         system_is_feasible = (state.mass_residual < 1e-8) and (state.largest_internal_cons_max_residual < 1e-9) and \
                              (chempot_diff < 1e-12) and (state.iteration > 5) and (largest_moles_change < 1e-9) and (phase_change_counter == 0)
-        #print(f'system_is_feasible={system_is_feasible}  state.mass_residual={state.mass_residual}  state.largest_internal_cons_max_residual={state.largest_internal_cons_max_residual}  chempot_diff={chempot_diff}  state.iteration={state.iteration}  largest_moles_change={largest_moles_change}  phase_change_counter={phase_change_counter}')
         if system_is_feasible:
             converged = True
             new_free_stable_compset_indices = np.array([i for i in range(state.phase_amt.shape[0])
