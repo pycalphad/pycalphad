pycalphad/pycalphad

commit db5086c3c47b21af1f82f24993237e98dcc16e46

ENH: New energy minimizer (#329)
* WIP: PhaseRecord: obj_parameters_2d function for fast calls over large groups of parameters

* WIP/FIX: PhaseRecord: obh_parameters_2d indexing error

* ENH: hyperplane: Performance improvements from profiling

* ENH: hyperplane: Turn off boundschecking for performance

* WIP: PhaseRecord: obj_parameters_2d function for fast calls over large groups of parameters

* WIP/FIX: PhaseRecord: obh_parameters_2d indexing error

* ENH: hyperplane: Performance improvements from profiling

* ENH: hyperplane: Turn off boundschecking for performance

* MNT: hyperplane: Reorganize arrays to be in dimension order

* WIP/ENH: hyperplane: Switch to malloc/free for some arrays

* WIP/ENH: hyperplane: Malloc/free support

* WIP: hyperplane: Type index variables for performance

* WIP: hyperplane: Switch to nogil

* WIP/ENH: hyperplane: Compute driving forces only for points at or below the candidate hyperplane

* MNT: hyperplane: Reorganize scalar declarations to be together

* MNT: hyperplane: Remove old todo

* calculate: Support vectors of parameters as argument

* WIP: SundmanSolver: An alternative solver based on Sundman et al, Comp. Mat. Sci., 2015.

* WIP: SundmanSolver: fix calculation of state variable indices and fix iterations at 50

* MAINT: calculate: Preserve interface for case that only one parameter value is specified

* WIP: SundmanSolver: Fix chemical potential conditions

* FIX: SundmanSolver: addition error for N=1 row

* WIP: SundmanSolver: Phase changing support

* WIP: SundmanSolver: Try freezing internal dof when new phase(s) added, until system balances mass again

* WIP: SundmanSolver: test improvements

* WIP: SundmanSolver: All but 6 tests pass. Use exact forms for N=1 constraint

* WIP: SundmanSolver: More test progress

* MAINT: SundmanSolver: Reorganize some solver functions into separate modules

* WIP: SundmanSolver: Fixes for most tests

pycalphad/core/lower_convex_hull.py
@@ -37,7 +37,7 @@ def lower_convex_hull(global_grid, state_variables, result_array):
     --------
     None yet.
     """
-    indep_conds = sorted([str(sv) for sv in state_variables])
+    indep_conds = sorted([str(sv) for sv in state_variables if str(sv) in result_array.coords.keys()])
     comp_conds = sorted([x for x in sorted(result_array.coords.keys()) if x.startswith('X_')])
     comp_conds_indices = sorted([idx for idx, x in enumerate(sorted(result_array.coords['component']))
                                  if 'X_'+x in comp_conds])
@@ -91,7 +91,16 @@ def lower_convex_hull(global_grid, state_variables, result_array):
     comp_coord_shape = tuple(len(result_array.coords[cond]) for cond in comp_conds)
     pot_coord_shape = tuple(len(result_array.coords[cond]) for cond in pot_conds)
     while not it.finished:
-        indep_idx = tuple(idx for idx, key in zip(it.multi_index, result_array_GM_dims) if key in indep_conds)
+        indep_idx = [] # tuple(idx for idx, key in zip(it.multi_index, result_array_GM_dims) if key in indep_conds)
+        # Relies on being ordered
+        for sv in state_variables:
+            if str(sv) in result_array.coords.keys():
+                coord_idx = list(result_array.coords.keys()).index(str(sv))
+                indep_idx.append(it.multi_index[coord_idx])
+            else:
+                # free state variable
+                indep_idx.append(0)
+        indep_idx = tuple(indep_idx)
         if len(comp_conds) > 0:
             comp_idx = np.ravel_multi_index(tuple(idx for idx, key in zip(it.multi_index, result_array_GM_dims) if key in comp_conds), comp_coord_shape)
             idx_comp_values = comp_values[comp_idx, :]

environment-dev.yml
@@ -10,7 +10,6 @@ dependencies:
   # `setup.py`). For example, conda-forge/symengine gives the C++ SymEngine
   # library, while conda-forge/python-symengine provides the Python package
   # called `symengine`.
-  - cyipopt>=1.0
   - cython>=0.24
   - matplotlib-base>=3.3
   - numpy>=1.13

pycalphad/codegen/callables.py
@@ -77,15 +77,15 @@ def build_callables(dbf, comps, phases, models, parameter_symbols=None,
         'massfuncs': {},
         'massgradfuncs': {},
         'masshessfuncs': {},
+        'formulamolefuncs': {},
+        'formulamolegradfuncs': {},
+        'formulamolehessfuncs': {},
         'callables': {},
         'grad_callables': {},
         'hess_callables': {},
         'internal_cons_func': {},
         'internal_cons_jac': {},
         'internal_cons_hess': {},
-        'multiphase_cons_func': {},
-        'multiphase_cons_jac': {},
-        'multiphase_cons_hess': {}
     }
 
     state_variables = get_state_variables(models=models)
@@ -130,6 +130,19 @@ def build_callables(dbf, comps, phases, models, parameter_symbols=None,
         _callables['massfuncs'][name] = mcf
         _callables['massgradfuncs'][name] = mgf
         _callables['masshessfuncs'][name] = mhf
+
+        # Build the callables for moles per formula unit
+        # TODO: In principle, we should also check for undefs in mod.moles()
+        fmcf, fmgf, fmhf = zip(*[build_functions(mod.moles(el, per_formula_unit=True), state_variables + site_fracs,
+                                                 include_obj=True,
+                                                 include_grad=build_gradients,
+                                                 include_hess=build_hessians,
+                                                 parameters=parameter_symbols)
+                                 for el in pure_elements])
+
+        _callables['formulamolefuncs'][name] = fmcf
+        _callables['formulamolegradfuncs'][name] = fmgf
+        _callables['formulamolehessfuncs'][name] = fmhf
     return {output: _callables}
 
 
@@ -188,9 +201,6 @@ def build_phase_records(dbf, comps, phases, conds, models, output='GM',
         'internal_cons_func': {},
         'internal_cons_jac': {},
         'internal_cons_hess': {},
-        'multiphase_cons_func': {},
-        'multiphase_cons_jac': {},
-        'multiphase_cons_hess': {}
     }
     phase_records = {}
     state_variables = sorted(get_state_variables(models=models, conds=conds), key=str)
@@ -200,8 +210,14 @@ def build_phase_records(dbf, comps, phases, conds, models, output='GM',
         callables = build_callables(dbf, comps, phases, models,
                                     parameter_symbols=parameters.keys(), output=output,
                                     additional_statevars=state_variables,
-                                    build_gradients=build_gradients,
-                                    build_hessians=build_hessians)
+                                    build_gradients=False,
+                                    build_hessians=False)
+    # Temporary solution. PhaseRecord needs rework: https://github.com/pycalphad/pycalphad/pull/329#discussion_r634579356
+    formulacallables = build_callables(dbf, comps, phases, models,
+                                       parameter_symbols=parameters.keys(), output='G',
+                                       additional_statevars=state_variables,
+                                       build_gradients=build_gradients,
+                                       build_hessians=build_hessians)
 
     # If a vector of parameters is specified, only pass the first row to the PhaseRecord
     # Future callers of PhaseRecord.obj_parameters_2d() can pass the full param_values array as an argument
@@ -212,31 +228,25 @@ def build_phase_records(dbf, comps, phases, conds, models, output='GM',
         mod = models[name]
         site_fracs = mod.site_fractions
         # build constraint functions
-        cfuncs = build_constraints(mod, state_variables + site_fracs, conds, parameters=param_symbols)
+        cfuncs = build_constraints(mod, state_variables + site_fracs, parameters=param_symbols)
         _constraints['internal_cons_func'][name] = cfuncs.internal_cons_func
         _constraints['internal_cons_jac'][name] = cfuncs.internal_cons_jac
         _constraints['internal_cons_hess'][name] = cfuncs.internal_cons_hess
-        _constraints['multiphase_cons_func'][name] = cfuncs.multiphase_cons_func
-        _constraints['multiphase_cons_jac'][name] = cfuncs.multiphase_cons_jac
-        _constraints['multiphase_cons_hess'][name] = cfuncs.multiphase_cons_hess
         num_internal_cons = cfuncs.num_internal_cons
-        num_multiphase_cons = cfuncs.num_multiphase_cons
 
         phase_records[name.upper()] = PhaseRecord(comps, state_variables, site_fracs, param_values,
                                                   callables[output]['callables'][name],
-                                                  callables[output]['grad_callables'][name],
-                                                  callables[output]['hess_callables'][name],
+                                                  formulacallables['G']['callables'][name],
+                                                  formulacallables['G']['grad_callables'][name],
+                                                  formulacallables['G']['hess_callables'][name],
                                                   callables[output]['massfuncs'][name],
-                                                  callables[output]['massgradfuncs'][name],
-                                                  callables[output]['masshessfuncs'][name],
+                                                  formulacallables['G']['formulamolefuncs'][name],
+                                                  formulacallables['G']['formulamolegradfuncs'][name],
+                                                  formulacallables['G']['formulamolehessfuncs'][name],
                                                   _constraints['internal_cons_func'][name],
                                                   _constraints['internal_cons_jac'][name],
                                                   _constraints['internal_cons_hess'][name],
-                                                  _constraints['multiphase_cons_func'][name],
-                                                  _constraints['multiphase_cons_jac'][name],
-                                                  _constraints['multiphase_cons_hess'][name],
-                                                  num_internal_cons,
-                                                  num_multiphase_cons)
+                                                  num_internal_cons)
 
         if verbose:
             print(name + ' ')

pycalphad/core/composition_set.pxd
@@ -6,13 +6,7 @@ cdef public class CompositionSet(object)[type CompositionSetType, object Composi
     cdef readonly double[::1] dof, X
     cdef double[:,::1] _X_2d_view
     cdef public double NP
-    cdef public int zero_seen
+    cdef public bint fixed
     cdef readonly double energy
     cdef double[::1] _energy_2d_view
-    cdef readonly double[::1] grad
-    cdef readonly double[::1] _prev_dof
-    cdef readonly double _prev_energy
-    cdef readonly double[::1] _prev_grad
-    cdef readonly bint _first_iteration
-    cdef void reset(self)
-    cdef void update(self, double[::1] site_fracs, double phase_amt, double[::1] state_variables, bint skip_derivatives) nogil
+    cpdef void update(self, double[::1] site_fracs, double phase_amt, double[::1] state_variables)

pycalphad/core/composition_set.pyx
@@ -16,65 +16,40 @@ cdef public class CompositionSet(object)[type CompositionSetType, object Composi
     """
     def __cinit__(self, PhaseRecord prx):
         self.phase_record = prx
-        self.zero_seen = 0
         self.dof = np.zeros(len(self.phase_record.variables)+len(self.phase_record.state_variables))
         self.X = np.zeros(len(self.phase_record.nonvacant_elements))
         self._X_2d_view = <double[:self.X.shape[0],:1]>&self.X[0]
         self.energy = 0
         self.NP = 0
+        self.fixed = False
         self._energy_2d_view = <double[:1]>&self.energy
-        self.grad = np.zeros(self.dof.shape[0])
-        self._prev_energy = 0
-        self._prev_dof = np.zeros(self.dof.shape[0])
-        self._prev_grad = np.zeros(self.dof.shape[0])
-        self._first_iteration = True
 
     def __deepcopy__(self, memodict=None):
         cdef CompositionSet other
         memodict = {} if memodict is None else memodict
         other = CompositionSet(self.phase_record)
         other.phase_record = self.phase_record
-        other.zero_seen = 0
         other.dof[:] = self.dof
         other.X[:] = self.X
-        other.mass_grad[:,:] = self.mass_grad
         other.energy = 1.0*self.energy
         other._energy_2d_view = <double[:1]>&other.energy
         other.NP = 1.0*self.NP
-        other.grad[:] = self.grad
-        other.hess[:,:] = self.hess
+        other.fixed = bool(self.fixed)
         return other
 
     def __repr__(self):
         return str(self.__class__.__name__) + "({0}, {1}, NP={2}, GM={3})".format(self.phase_record.phase_name,
                                                                           np.asarray(self.X), self.NP, self.energy)
 
-    cdef void reset(self):
-        self.zero_seen = 0
-        self._prev_energy = 0
-        self._prev_dof[:] = 0
-        self._prev_grad[:] = 0
-        self._first_iteration = True
-
-
     @cython.boundscheck(False)
     @cython.wraparound(False)
-    cdef void update(self, double[::1] site_fracs, double phase_amt, double[::1] state_variables, bint skip_derivatives) nogil:
+    cpdef void update(self, double[::1] site_fracs, double phase_amt, double[::1] state_variables):
         cdef int comp_idx
         self.dof[:state_variables.shape[0]] = state_variables
         self.dof[state_variables.shape[0]:] = site_fracs
         self.NP = phase_amt
         self.energy = 0
-        memset(&self.grad[0], 0, self.grad.shape[0] * sizeof(double))
         memset(&self.X[0], 0, self.X.shape[0] * sizeof(double))
         self.phase_record.obj(self._energy_2d_view, self.dof)
-        if not skip_derivatives:
-            self.phase_record.grad(self.grad, self.dof)
         for comp_idx in range(self.X.shape[0]):
             self.phase_record.mass_obj(self._X_2d_view[comp_idx], self.dof, comp_idx)
-        if not skip_derivatives:
-            if self._first_iteration == True:
-                self._prev_dof[:] = self.dof
-                self._prev_energy = self.energy
-                self._prev_grad[:] = self.grad
-                self._first_iteration = False

pycalphad/core/constants.py
@@ -4,13 +4,11 @@ in the module.
 Note that modifying these may yield unpredictable results.
 """
 # Force zero values to this amount, for numerical stability
-MIN_SITE_FRACTION = 1e-15
+MIN_SITE_FRACTION = 8e-13
 MIN_PHASE_FRACTION = 1e-6
 # Phases with mole fractions less than COMP_DIFFERENCE_TOL apart (by Chebyshev distance) are considered "the same" for
 # the purposes of CompositionSet addition and removal during energy minimization.
 COMP_DIFFERENCE_TOL = 1e-4
 
 # Constraint scaling factors, for numerical stability
-CHEMPOT_CONSTRAINT_SCALING = 1.0
-MULTIPHASE_CONSTRAINT_SCALING = 1e6
-INTERNAL_CONSTRAINT_SCALING = 1e6
+INTERNAL_CONSTRAINT_SCALING = 1.0

pycalphad/core/constraints.py
@@ -1,44 +1,21 @@
-from symengine import Symbol
-from pycalphad.core.constants import INTERNAL_CONSTRAINT_SCALING, MULTIPHASE_CONSTRAINT_SCALING
+from pycalphad.core.constants import INTERNAL_CONSTRAINT_SCALING
 from pycalphad.codegen.sympydiff_utils import build_constraint_functions
 from collections import namedtuple
 
 ConstraintTuple = namedtuple('ConstraintTuple', ['internal_cons_func', 'internal_cons_jac', 'internal_cons_hess',
-                                                 'multiphase_cons_func', 'multiphase_cons_jac', 'multiphase_cons_hess',
-                                                 'num_internal_cons', 'num_multiphase_cons'])
+                                                 'num_internal_cons'])
 
 
-def is_multiphase_constraint(cond):
-    cond = str(cond)
-    if cond == 'N' or cond.startswith('X_'):
-        return True
-    else:
-        return False
-
-
-def build_constraints(mod, variables, conds, parameters=None):
+def build_constraints(mod, variables, parameters=None):
     internal_constraints = mod.get_internal_constraints()
     internal_constraints = [INTERNAL_CONSTRAINT_SCALING*x for x in internal_constraints]
-    multiphase_constraints = mod.get_multiphase_constraints(conds)
-    multiphase_constraints = [MULTIPHASE_CONSTRAINT_SCALING*x for x in multiphase_constraints]
+
     cf_output = build_constraint_functions(variables, internal_constraints,
                                            parameters=parameters)
     internal_cons_func = cf_output.cons_func
     internal_cons_jac = cf_output.cons_jac
     internal_cons_hess = cf_output.cons_hess
 
-    result_build = build_constraint_functions(variables + [Symbol('NP')],
-                                              multiphase_constraints,
-                                              parameters=parameters)
-    multiphase_cons_func = result_build.cons_func
-    multiphase_cons_jac = result_build.cons_jac
-    multiphase_cons_hess = result_build.cons_hess
     return ConstraintTuple(internal_cons_func=internal_cons_func, internal_cons_jac=internal_cons_jac,
                            internal_cons_hess=internal_cons_hess,
-                           multiphase_cons_func=multiphase_cons_func, multiphase_cons_jac=multiphase_cons_jac,
-                           multiphase_cons_hess=multiphase_cons_hess,
-                           num_internal_cons=len(internal_constraints), num_multiphase_cons=len(multiphase_constraints))
-
-
-def get_multiphase_constraint_rhs(conds):
-    return [MULTIPHASE_CONSTRAINT_SCALING*float(value) for cond, value in conds.items() if is_multiphase_constraint(cond)]
+                           num_internal_cons=len(internal_constraints))

commit 6219d4d2643f691891eee8b946c5148806057b21

FIX: potential sorting bug in lower_convex_hull (#339)
Sort the `state_variables` in `lower_convex_hull` to fix a sorting bug introduced by #329.

We used to rely on `indep_conds` for sorting the state variables, but now we use the `state_variables` directly and `indep_conds` is no longer used.

pycalphad/core/lower_convex_hull.py
@@ -18,7 +18,7 @@ def lower_convex_hull(global_grid, state_variables, result_array):
     ----------
     global_grid : Dataset
         A sample of the energy surface of the system.
-    state_variables : list
+    state_variables : List[v.StateVariable]
         A list of the state variables (e.g., P, T) used in this calculation.
     result_array : Dataset
         This object will be modified!
@@ -37,7 +37,7 @@ def lower_convex_hull(global_grid, state_variables, result_array):
     --------
     None yet.
     """
-    indep_conds = sorted([str(sv) for sv in state_variables if str(sv) in result_array.coords.keys()])
+    state_variables = sorted(state_variables, key=str)
     comp_conds = sorted([x for x in sorted(result_array.coords.keys()) if x.startswith('X_')])
     comp_conds_indices = sorted([idx for idx, x in enumerate(sorted(result_array.coords['component']))
                                  if 'X_'+x in comp_conds])
@@ -91,7 +91,7 @@ def lower_convex_hull(global_grid, state_variables, result_array):
     comp_coord_shape = tuple(len(result_array.coords[cond]) for cond in comp_conds)
     pot_coord_shape = tuple(len(result_array.coords[cond]) for cond in pot_conds)
     while not it.finished:
-        indep_idx = [] # tuple(idx for idx, key in zip(it.multi_index, result_array_GM_dims) if key in indep_conds)
+        indep_idx = []
         # Relies on being ordered
         for sv in state_variables:
             if str(sv) in result_array.coords.keys():