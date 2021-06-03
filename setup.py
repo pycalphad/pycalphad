e19d104d806b5e58c00293acead36bea7e679b25
bocklund <brandonbocklund@gmail.com>
2021-06-03 09:25:44 -0400

    Drop Python 3.6 (following NEP 29)

--- setup.py
+++ setup.py
@@ -83,10 +83,8 @@ setup(
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
+        # Supported Python versions
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
