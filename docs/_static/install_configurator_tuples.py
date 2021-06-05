ceeb94125c4dfe6f095546950ebded6c5286200e
bocklund <brandonbocklund@gmail.com>
2021-06-05 13:02:02 -0400

    Clean up config generator

--- docs/_static/install_configurator_tuples.py
+++ docs/_static/install_configurator_tuples.py
@@ -1,20 +1,26 @@
 """
 This module print out static HTML elements for hardcoded installation instructions
 """
-package_managers = ['pip', 'conda', 'source']
-platform = ['linux', 'windows']
-env_flag = ['noenv', 'env']
-jupyter_flag = ['nojupyter', 'jupyter']
+
 import itertools
+from typing import List
 
+# Hardcoded content
 ENVIRONMENT_NAME = 'pycalphad-env'
+# For integration into external tools, like Sphinx:
+HTML_TEMPLATE = """<div id={} hidden><div class="highlight-bash notranslate"><div class="highlight"><pre>{}</pre></div></div></div>"""
+ENTRIES_JOIN_STR = ""  # join between different HTML entries
 
-for config_tuple in itertools.product(package_managers, platform, env_flag, jupyter_flag):
-    pkg, plt, env, jup = config_tuple
-    name = '_'.join(config_tuple)
+# Matrix of options
+PACKAGE_MANAGERS = ['pip', 'conda', 'source']
+PLATFORMS = ['linux', 'windows']
+ENV_FLAGS = ['noenv', 'env']
+JUPYTER_FLAGS = ['nojupyter', 'jupyter']
 
+
+def get_config_install_lines(pkg, plt, env, jup) -> List[str]:
+    """Take the configuration and produce a list of lines to install that configuration"""
     install_lines = []
-    prefmt_string = ''
 
     if env == 'env':
         # Create environment
@@ -34,7 +40,6 @@ for config_tuple in itertools.product(package_managers, platform, env_flag, jupy
 
     # Install pycalphad with optional JupyterLab
     # Should not end with newline
-    install_string = ""
     if pkg == 'conda':
         install_line = "conda install pycalphad"
         if jup == "jupyter":
@@ -51,15 +56,30 @@ for config_tuple in itertools.product(package_managers, platform, env_flag, jupy
         if jup == "jupyter":
             install_line += " jupyterlab"
         install_lines.append(install_line)
+    return install_lines
 
-    # prepend "$ " to lines
-    if plt == 'windows':
-        install_lines = ["> " + line for line in install_lines]
-    else:
-        install_lines = ["$ " + line for line in install_lines]
 
-    preformatted_string = "\n".join(install_lines)
-    s = f'<pre id={name} hidden>{preformatted_string}</pre>'
-    print(s)
-    print()
-#    print('_'.join(config_tuple))
+def get_config_html(package_manager, platform, env_flag, jupyter_flag, prepend_shell_prompt=True):
+    """Get HTML for a single configuration"""
+    install_lines = get_config_install_lines(package_manager, platform, env_flag, jupyter_flag)
+
+    if prepend_shell_prompt:
+        if platform == 'windows':
+            install_lines = ["> " + line for line in install_lines]
+        else:
+            install_lines = ["$ " + line for line in install_lines]
+
+    # Build the HTML
+    config_id = '_'.join((package_manager, platform, env_flag, jupyter_flag))
+    formatted_string = "\n".join(install_lines)
+    generated_config_html = HTML_TEMPLATE.format(config_id, formatted_string)
+    return generated_config_html
+
+def get_matrix_html(package_managers, platform, env_flags, jupyter_flags):
+    html_entries = []
+    for config_tuple in itertools.product(package_managers, platform, env_flags, jupyter_flags):
+        html_entries.append(get_config_html(*config_tuple))
+    return ENTRIES_JOIN_STR.join(html_entries)
+
+if __name__ == '__main__':
+    print(get_matrix_html(PACKAGE_MANAGERS, PLATFORMS, ENV_FLAGS, JUPYTER_FLAGS))
