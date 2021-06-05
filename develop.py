10225c49884efbe2acad7d41a11135d15985734b
Richard Otis <rotis@jpl.nasa.gov>
2021-06-05 12:06:31 -0700

    fixup

--- develop.py
+++ develop.py
@@ -8,5 +8,5 @@ path_to_develop = pathlib.Path(__file__).parent.absolute()
 c = toml.load(path_to_develop / "pyproject.toml")
 build_deps = c["build-system"]["requires"]
 if __name__ == '__main__':
-    subprocess.run([sys.executable, "-m", "pip", "install", "--quiet"] + build_deps)
+    subprocess.run([sys.executable, "-m", "pip", "install"] + build_deps)
     subprocess.run([sys.executable, "-m", "pip", "install", "--no-build-isolation", "--editable", str(path_to_develop)])
