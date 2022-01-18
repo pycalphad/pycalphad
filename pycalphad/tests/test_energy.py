f768e9256944859b84766413daca1ba3c87da8d6
Brandon Bocklund <bocklund1@llnl.gov>
2022-01-18 11:56:24 -0800

    Skip failing Thermochimica tests for (Fe2,Fe3,Sb)(O,S) with new FactSage result
    Thanks to Max Poschmann for running the calculation for us.

--- pycalphad/tests/test_energy.py
+++ pycalphad/tests/test_energy.py
@@ -748,7 +748,7 @@ def test_MQMQA_SUBQ_entropy_only(load_database):
     assert np.isclose(float(mod.moles("O").subs(subs_dict)), 0.3, 1e-5)
     assert np.isclose(float(mod.moles("S").subs(subs_dict)), 0.3, 1e-5)
     
-# TODO: make this pass
+@pytest.mark.skip(reason='Thermochimica seems to produce the wrong answers currently.')
 @select_database("Shishin_Fe-Sb-O-S_slag.dat")
 def test_MQMQA_SUBQ_Q_mixing_1000K(load_database):
     dbf = load_database()
@@ -794,7 +794,53 @@ def test_MQMQA_SUBQ_Q_mixing_1000K(load_database):
     assert np.isclose(float(mod.moles("O").subs(subs_dict)), 0.3, 1e-5)
     assert np.isclose(float(mod.moles("S").subs(subs_dict)), 0.3, 1e-5)
 
+@select_database("Shishin_Fe-Sb-O-S_slag.dat")
+def test_MQMQA_SUBQ_Q_mixing_1000K_FACTSAGE(load_database):
+    dbf = load_database()
+    # TODO: demonstration of bad species mangling
+    FE2 = v.Species("FE2++2.0", constituents={"FE": 2.0}, charge=2)
+    FE3 = v.Species("FE3++3.0", constituents={"FE": 3.0}, charge=3)
+    SB3 = v.Species("SB3++3.0", constituents={"SB": 3.0}, charge=3)
+    O = v.Species("O-2.0", constituents={"O": 1.0}, charge=-2)
+    S = v.Species("S-2.0", constituents={"S": 1.0}, charge=-2)
+    mod = ModelMQMQA(dbf, ["FE", "SB", "O", "S"], "SLAG-LIQ")
 
+    assert FE2 in mod.cations
+    assert FE3 in mod.cations
+    assert SB3 in mod.cations
+    assert O in mod.anions
+    assert S in mod.anions
+
+    subs_dict = {  # FactSage site fractions (Fe2 quadruplet fractions not printed, I assumed 1e-30)
+        mod._X_ijkl(FE2,FE2,O,O): 1e-30,
+        mod._X_ijkl(FE3,FE3,O,O): 5.5018E-03,
+        mod._X_ijkl(SB3,SB3,O,O): 0.26528,
+        mod._X_ijkl(FE2,FE3,O,O): 1e-30,
+        mod._X_ijkl(FE2,SB3,O,O): 1e-30,
+        mod._X_ijkl(FE3,SB3,O,O): 7.6407E-02,
+        mod._X_ijkl(FE2,FE2,S,S): 1e-30,
+        mod._X_ijkl(FE3,FE3,S,S): 0.26528,
+        mod._X_ijkl(SB3,SB3,S,S): 5.5018E-03,
+        mod._X_ijkl(FE2,FE3,S,S): 1e-30,
+        mod._X_ijkl(FE2,SB3,S,S): 1e-30,
+        mod._X_ijkl(FE3,SB3,S,S): 7.6407E-02,
+        mod._X_ijkl(FE2,FE2,O,S): 1e-30,
+        mod._X_ijkl(FE3,FE3,O,S): 7.6407E-02,
+        mod._X_ijkl(SB3,SB3,O,S): 7.6407E-02,
+        mod._X_ijkl(FE2,FE3,O,S): 1e-30,
+        mod._X_ijkl(FE2,SB3,O,S): 1e-30,
+        mod._X_ijkl(FE3,SB3,O,S): 0.15281,
+        v.T: 1000.0,
+    }
+    print(mod.GM.subs(subs_dict))
+    check_energy(mod, subs_dict, -131831.0, mode="sympy")  # FactSage energy, from Max
+    assert np.isclose(float(mod.moles("FE").subs(subs_dict)), 0.2, 1e-5)
+    assert np.isclose(float(mod.moles("SB").subs(subs_dict)), 0.2, 1e-5)
+    assert np.isclose(float(mod.moles("O").subs(subs_dict)), 0.3, 1e-5)
+    assert np.isclose(float(mod.moles("S").subs(subs_dict)), 0.3, 1e-5)
+
+
+@pytest.mark.skip(reason='Thermochimica seems to produce the wrong answers currently.')
 @select_database("Shishin_Fe-Sb-O-S_slag.dat")
 def test_MQMQA_SUBQ_Q_mixing_400K(load_database):
     dbf = load_database()
