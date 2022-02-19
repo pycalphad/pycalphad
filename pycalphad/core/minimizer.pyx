9fa7c2aa3a9835702e7ccd733ec1bcd57b7eb929
Richard Otis <rotis@jpl.nasa.gov>
2022-02-19 09:35:04 -0800

    WIP: minimizer: Try increasing ALLOWED_DELTA_Y a little more

--- pycalphad/core/minimizer.pyx
+++ pycalphad/core/minimizer.pyx
@@ -813,7 +813,7 @@ cpdef find_solution(list compsets, int num_statevars, int num_components,
     cdef SystemState state = SystemState(spec, compsets)
 
     # convergence criteria
-    cdef double ALLOWED_DELTA_Y = 5e-09
+    cdef double ALLOWED_DELTA_Y = 8e-09
     cdef double ALLOWED_DELTA_PHASE_AMT = 1e-10
     cdef double ALLOWED_DELTA_STATEVAR = 1e-5  # changes defined as percent change
 
