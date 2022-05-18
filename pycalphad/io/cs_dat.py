54305ec97f46e5d04f8e58d3265c437341089808
Brandon Bocklund <bocklund1@llnl.gov>
2022-05-18 15:32:09 -0700

    Cleanup fix and add a comment to the ModelMQMQA.Z method

--- pycalphad/io/cs_dat.py
+++ pycalphad/io/cs_dat.py
@@ -573,15 +573,17 @@ class SUBQQuadrupletCoordinations:
         """Add a Z_i_AB:XY parameter for each species defined in the quadruplet"""
         linear_species = [''] + As + Xs  # the leading '' element pads for one-indexed quadruplet_idxs
         A, B, X, Y = tuple(linear_species[idx] for idx in self.quadruplet_idxs)
-        true_Z=self.quadruplet_coordinations
-        if A!=sorted((A, B))[0]:
-            true_Z[0],true_Z[1]=true_Z[1],true_Z[0]
-        if X!=sorted((X, Y))[0]:
-            true_Z[2],true_Z[3]=true_Z[3],true_Z[2]
-        constituent_array = [sorted((A, B)), sorted((X, Y))]
+        Z_A, Z_B, Z_X, Z_Y = self.quadruplet_coordinations
+        # Constituents and coordinations need to be canonically sorted (within each sublattice)
+        constituent_array = []  # Should be split by sublattice, List[List[float]]
+        coordinations = []  # Should be "linear", List[float]
+        for const_subl, coord_subl in zip([[A, B], [X, Y]], [[Z_A, Z_B], [Z_X, Z_Y]]):
+            sorted_const, sorted_coord = zip(*[(const, coord) for const, coord in sorted(zip(const_subl, coord_subl))])
+            constituent_array.append(list(sorted_const))
+            coordinations.extend(sorted_coord)
         dbf.add_parameter(
             "MQMZ", phase_name, constituent_array, param_order=None, param=None,
-            coordinations=true_Z, force_insert=False,
+            coordinations=coordinations, force_insert=False,
             )
 
 
