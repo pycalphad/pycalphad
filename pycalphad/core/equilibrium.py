908e8633d30b00d46acf45740ac967bad07c7eac
Richard Otis <rotis@jpl.nasa.gov>
2022-07-28 10:01:45 -0700

    WIP: equilibrium: Add sketch of dot_derivative function

--- pycalphad/core/equilibrium.py
+++ pycalphad/core/equilibrium.py
@@ -17,6 +17,7 @@ from pycalphad.core.eqsolver import _solve_eq_at_conditions
 from pycalphad.core.phase_rec import PhaseRecord
 from pycalphad.core.composition_set import CompositionSet
 from pycalphad.core.solver import Solver
+from pycalphad.core.minimizer import site_fraction_differential, state_variable_differential
 from pycalphad.core.light_dataset import LightDataset
 from pycalphad.model import Model
 import numpy as np
@@ -39,6 +40,45 @@ def _adjust_conditions(conds):
         else:
             new_conds[key] = unpack_condition(value)
     return new_conds
+
+
+def dot_derivative(spec, state, property_records):
+    """
+    Sample the internal degrees of freedom of a phase.
+
+    Parameters
+    ----------
+    spec : SystemSpecifications
+        some description
+    state : Title
+        another description
+
+    Returns
+    -------
+    dot derivative of property
+    """
+    statevar_of_interest = v.T
+    state_variables = state.compsets[0].phase_record.state_variables
+    statevar_idx = sorted(state_variables, key=str).index(statevar_of_interest)
+    delta_chemical_potentials, delta_statevars, delta_phase_amounts = \
+    state_variable_differential(spec, state, statevar_idx)
+
+    # Sundman et al, 2015, Eq. 73
+    dot_derivative = 0.0
+    naive_derivative = 0.0
+    for idx, compset in enumerate(state.compsets):
+        phase_name = compset.phase_record.phase_name
+        proprecord = property_records[phase_name]
+        func_value, grad_value = proprecord.func(compset.dof), proprecord.grad(compset.dof)
+        delta_sitefracs = site_fraction_differential(state.cs_states[idx], delta_chemical_potentials,
+                                                     delta_statevars)
+
+        dot_derivative += delta_phase_amounts[idx] * func_value
+        dot_derivative += compset.NP * grad_value[statevar_idx] * delta_statevars[statevar_idx]
+        naive_derivative += compset.NP * grad_value[statevar_idx] * delta_statevars[statevar_idx]
+        dot_derivative += compset.NP * np.dot(delta_sitefracs, grad_value[len(state_variables):])
+
+    return dot_derivative
 
 
 def apply_to_dataset(input_dataset, phase_records, function_to_apply, per_phase=False, fill_value=np.nan, dtype=float):
