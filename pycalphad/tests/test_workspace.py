23933be0210d18b3ea2479e63dd9b8ffd66af0fd
Richard Otis <rotis@jpl.nasa.gov>
2022-10-22 17:52:21 -0700

    FIX/TST: Workspace: Add tzero test

--- pycalphad/tests/test_workspace.py
+++ pycalphad/tests/test_workspace.py
@@ -1,6 +1,8 @@
 from numpy.testing import assert_allclose
+import numpy as np
 from pycalphad import Workspace, variables as v
 from pycalphad.property_framework import as_property, ComputableProperty, T0, IsolatedPhase, DormantPhase
+from pycalphad.property_framework.units import Q_
 from pycalphad.tests.fixtures import load_database, select_database
 import pytest
 
@@ -14,12 +16,27 @@ def test_workspace_creation(load_database):
                      {v.N: 1, v.P: 1e5, v.T: (300, 1000, 10), v.X('ZN'): 0.3})
     assert_allclose(wks.eq.GM, wks2.eq.GM)
 
-def test_meta_property_creation():
-    pass
-
 @select_database("alzn_mey.tdb")
-def test_tzero_property_creation(load_database):
+def test_meta_property_creation(load_database):
     dbf = load_database()
     wks = Workspace(dbf=dbf, comps=['AL', 'ZN', 'VA'], phases=['FCC_A1', 'HCP_A3', 'LIQUID'],
-                    conditions={v.N: 1, v.P: 1e5, v.T: (300, 1000, 1), v.X('ZN'): 0.3})
-    assert isinstance(T0('FCC_A1', 'HCP_A3', wks=wks), ComputableProperty)
+                    conditions={v.N: 1, v.P: 1e5, v.T: (300, 1000, 10), v.X('ZN'): 0.3})
+    my_tzero = T0('FCC_A1', 'HCP_A3', wks=wks)
+    assert isinstance(my_tzero, ComputableProperty)
+
+@select_database("alzn_mey.tdb")
+def test_tzero_property(load_database):
+    dbf = load_database()
+    wks = Workspace(dbf=dbf, comps=['AL', 'ZN', 'VA'], phases=['FCC_A1', 'HCP_A3', 'LIQUID'],
+                    conditions={v.N: 1, v.P: 1e5, v.T: 600, v.X('ZN'): (0.01,1-0.01,0.01)})
+    my_tzero = T0('FCC_A1', 'HCP_A3', wks=wks)
+    assert isinstance(my_tzero, ComputableProperty)
+    assert my_tzero.property_to_optimize == v.T
+    t0_values, = wks.get(my_tzero)
+    assert_allclose(np.nanmax(t0_values.magnitude), 2952.90443)
+    wks.conditions[v.X('ZN')] = 0.3
+    my_tzero.property_to_optimize = v.X('ZN')
+    my_tzero.minimum_value = 0.0
+    my_tzero.maximum_value = 1.0
+    t0_composition, = wks.get(my_tzero)
+    assert_allclose(t0_composition[0].magnitude, Q_(0.86119, 'fraction').magnitude, atol=my_tzero.residual_tol)
