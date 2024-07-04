da2bbbbf71e068a45179b199d6abcae9d21dfbf8
Richard Otis <richard.otis@outlook.com>
2024-07-04 12:44:37 -0500

    MAINT/BLD: Drop Python 3.8 support (#537)

--- setup.py
+++ setup.py
@@ -59,7 +59,6 @@ setup(
         # gives the C++ SymEngine library, while conda-forge/python-symengine
         # provides the Python package called `symengine`.
         'Cython' if os.getenv('CYTHON_COVERAGE', False) else '', # required for Cython test coverage
-        'importlib_resources',  # drop when pycalphad drops support for Python<3.9
         'matplotlib>=3.3',
         'numpy>=1.13',
         'pyparsing>=2.4',
@@ -88,7 +87,6 @@ setup(
 
         # Supported Python versions
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
