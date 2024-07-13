8d976d5083523309241dd4ebf29e78c8f917cff8
Richard Otis <richard.otis@outlook.com>
2024-07-13 18:14:04 -0500

    WIP: minimizer: Guard lapack calls from NaN

--- pycalphad/core/minimizer.pyx
+++ pycalphad/core/minimizer.pyx
@@ -5,6 +5,7 @@ from pycalphad.core.composition_set cimport CompositionSet
 from pycalphad.core.constants import MIN_SITE_FRACTION
 cimport scipy.linalg.cython_lapack as cython_lapack
 from libc.stdlib cimport malloc, free
+from libc.math cimport isnan
 
 @cython.boundscheck(False)
 cdef void lstsq(double *A, int M, int N, double* x, double rcond) nogil:
@@ -19,9 +20,18 @@ cdef void lstsq(double *A, int M, int N, double* x, double rcond) nogil:
     cdef int rank = 0
     cdef double* work = <double*>malloc(lwork * sizeof(double))
     cdef double* singular_values = <double*>malloc(N * sizeof(double))
+    cdef bint isfinite = True
 
-    cython_lapack.dgelsd(&M, &N, &NRHS, A, &N, x, &M, singular_values, &rcond, &rank,
-                         work, &lwork, &iwork, &info)
+    for i in range(M*N):
+        if isnan(A[i]):
+            isfinite = False
+
+    if not isfinite:
+        for i in range(N):
+            x[i] = 0
+    else:
+        cython_lapack.dgelsd(&M, &N, &NRHS, A, &N, x, &M, singular_values, &rcond, &rank,
+                            work, &lwork, &iwork, &info)
     free(singular_values)
     free(work)
     if info != 0:
@@ -43,10 +53,21 @@ cpdef void lstsq_check_infeasible(double[:,::] A, double[::] b, double[::] out_x
 cdef void invert_matrix(double *A, int N, int* ipiv) nogil:
     "A will be overwritten."
     cdef int info = 0
+    cdef int i
     cdef double* work = <double*>malloc(N * sizeof(double))
+    cdef bint isfinite = True
 
-    cython_lapack.dgetrf(&N, &N, A, &N, ipiv, &info)
-    cython_lapack.dgetri(&N, A, &N, ipiv, work, &N, &info)
+    for i in range(N**2):
+        if isnan(A[i]):
+            isfinite = False
+
+    if not isfinite:
+        for i in range(N**2):
+            A[i] = 0
+    else:
+        cython_lapack.dgetrf(&N, &N, A, &N, ipiv, &info)
+        cython_lapack.dgetri(&N, A, &N, ipiv, work, &N, &info)
+
     free(work)
     if info != 0:
         for i in range(N**2):
