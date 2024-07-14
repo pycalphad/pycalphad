removed unused DOO+composition heuristic for sorting/fixing composition sets in starting points. Sorting/fixing composition sets is now determined from Jansson derivatives

pycalphad/mapping/starting_points.py
@@ -12,24 +12,11 @@ import pycalphad.mapping.utils as map_utils
 
 _log = logging.getLogger(__name__)
 
-def _sort_cs_to_fix(comp_sets: list[CompositionSet], conditions: dict[v.StateVariable, float], chemical_potentials: list[float]):
-    """
-    1. DOO
-    2. Composition product
-    """
-    doo, prod = [], []
-    for cs in comp_sets:
-        doo.append(as_property(f"degree_of_ordering({cs.phase_record.phase_name})").compute_property([cs], conditions, chemical_potentials))
-        if doo[-1] < 1e-3:
-            doo[-1] = 0
-        prod.append(np.prod(cs.X))
-    
-    for i in range(len(comp_sets)):
-        for j in range(i+1, len(comp_sets)):
-            if (doo[j] > doo[i]) or (abs(doo[j] - doo[i]) < 1e-3 and prod[j] < prod[i]):
-                comp_sets[i], comp_sets[j] = comp_sets[j], comp_sets[i]
+"""
+Functions for generating starting points from equilibrium
+"""
 
-def point_from_equilibrium(dbf: Database, components: list[str], phases: list[str], conditions: dict[v.StateVariable, float], free_var: Union[v.StateVariable, list[v.StateVariable]] = None, **eq_kwargs):
+def point_from_equilibrium(dbf: Database, components: list[str], phases: list[str], conditions: dict[v.StateVariable, float], **eq_kwargs):
     """
     1. Computes equilibrium with dbf, components, phases and conditions
     2. Free selected variables (if any), fixing composition sets by some heuristic on which CS should be fixed first (generally refers to CS with least degrees of freedom)
@@ -44,18 +31,7 @@ def point_from_equilibrium(dbf: Database, components: list[str], phases: list[st
     if len(comp_sets) == 0:
         return None
 
-    _sort_cs_to_fix(comp_sets, conditions, chemical_potentials)
-
     copy_conds = copy.deepcopy(conditions)
-
-    if free_var is not None:
-        if not hasattr(free_var, "len"):
-            free_var = [free_var]
-
-        for i in range(len(free_var)):
-            comp_sets[i].fixed = True
-            map_utils.update_cs_phase_frac(comp_sets[i], 0.0)
-            del copy_conds[free_var[i]]
 
     np_sum = sum([cs.NP for cs in comp_sets])
     for cs in comp_sets:
@@ -66,36 +42,6 @@ def point_from_equilibrium(dbf: Database, components: list[str], phases: list[st
 
     point = Point(copy_conds, chemical_potentials, [cs for cs in comp_sets if cs.fixed], [cs for cs in comp_sets if not cs.fixed])
     return point
-
-def create_point_with_free_variable(point: Point, free_var: Union[v.StateVariable, list[v.StateVariable]] = None):
-    """
-    Creates new point, freeing up variables and fixing composition sets of previous point
-
-    Note: this currently assumes that all composition sets in point are free
-    """
-    comp_sets = point.stable_composition_sets
-
-    _sort_cs_to_fix(comp_sets, point.global_conditions, point.chemical_potentials)
-    copy_conds = copy.deepcopy(point.global_conditions)
-
-    if free_var is not None:
-        if not hasattr(free_var, "len"):
-            free_var = [free_var]
-
-        for i in range(len(free_var)):
-            comp_sets[i].fixed = True
-            map_utils.update_cs_phase_frac(comp_sets[i], 0.0)
-            del copy_conds[free_var[i]]
-
-    np_sum = sum([cs.NP for cs in comp_sets])
-    for cs in comp_sets:
-        map_utils.update_cs_phase_frac(cs, cs.NP/np_sum)
-
-    for key in copy_conds:
-        copy_conds[key] = key.compute_property(comp_sets, copy_conds, point.chemical_potentials)
-
-    new_point = Point(copy_conds, point.chemical_potentials, [cs for cs in comp_sets if cs.fixed], [cs for cs in comp_sets if not cs.fixed])
-    return new_point
         
 
 

pycalphad/mapping/utils.py
@@ -8,16 +8,67 @@ from pycalphad.core.composition_set import CompositionSet
 from pycalphad.mapping.primitives import Point, Node, STATEVARS
 
 def degrees_of_freedom(point: Union[Point, Node], components: list[str], num_potential_conditions: int):
+    """
+    Degrees of freedom of point defined by Gibbs phase rule
+
+    Components + 2 = Phases + DOF
+        2 is offsetted by the number of potential conditions
+
+    Parameters
+    ----------
+    point : Point or Node
+    components : [str]
+        List of components, VA will be ignored
+    num_potential_conditions : number of variable potential conditions
+
+    Returns
+    -------
+    int - degrees of freedom, 0 represents an invariant point/node
+    """
     non_va_comps = len(set(components) - {"VA"})
     return non_va_comps + 2 - len(point.stable_composition_sets) - (2 - num_potential_conditions)
 
 def update_cs_phase_frac(comp_set: CompositionSet, phase_frac: float):
+    """
+    Wrapper to update the phase fraction of a composition set
+    This just helps with splitting the cs.dof to state variables and constituents when updating
+
+    Parameters
+    ----------
+    comp_set : CompositionSet
+    phase_frac : float
+    """
     comp_set.update(comp_set.dof[len(STATEVARS):], phase_frac, comp_set.dof[:len(STATEVARS)])
 
 def get_statevars_array(conditions: dict[v.StateVariable, float]):
+    """
+    Creates numpy array of state variables in conditions
+        Sorted by STATEVARS (N, P, T)
+
+    Parameters
+    ----------
+    conditions : dict[v.StateVariable, float]
+
+    Returns
+    -------
+    numpy array of len(STATEVARS)
+    """
     return np.asarray([conditions[sv] for sv in STATEVARS], dtype=np.float64)
 
 def elements_from_components(components: list[str]):
+    """
+    Extracts all pure elements from components
+    NOTE: not super useful and may change when component/species conditions are better supported
+          Currently, mapping assumes pure element conditions
+
+    Parameters
+    ----------
+    components : [str]
+
+    Returns
+    -------
+    elements : [str]
+    """
     # Components can be a compound, so we want to get all unique elements
     # excluding VA. We'll also sort them
     non_va_comps = list(set(components) - {"VA"})
@@ -26,11 +77,38 @@ def elements_from_components(components: list[str]):
     return sorted(list(elements))
 
 def _sort_axis_by_state_vars(axis_vars: list[v.StateVariable]):
+    """
+    Sorts list of axis variables by [state variables (N, P, T)] + [non-state variables]
+
+    Parameters
+    ----------
+    axis_vars : [v.StateVariable]
+
+    Returns
+    -------
+    sorted axis vars : [v.StateVariable]
+    """
     state_vars = [av for av in axis_vars if av in STATEVARS]
     non_state_vars = [av for av in axis_vars if av not in STATEVARS]
     return state_vars + non_state_vars
 
 def _generate_point_with_fixed_cs(point: Point, cs_to_fix: CompositionSet, cs_to_free: CompositionSet):
+    """
+    Generates point with two cs, one fixed and one free
+
+    Parameters
+    ----------
+    point : Point
+        Point to generate new point from
+    cs_to_fix : CompositionSet
+    cs_to_free : CompositionSet
+
+    Returns
+    -------
+    new_point : Point
+        Retains same equilibria as previous point
+        Conditions will be updated with the updated phase fractions of the composition sets
+    """
     new_point = Point.with_copy(point.global_conditions, point.chemical_potentials, [cs_to_fix], [cs_to_free])
 
     new_point._fixed_composition_sets[0].fixed = True
@@ -42,10 +120,22 @@ def _generate_point_with_fixed_cs(point: Point, cs_to_fix: CompositionSet, cs_to
         new_point.global_conditions[key] = new_point.get_property(key)
     return new_point
 
-def _generate_point_with_free_cs(point: Point, bias_towards_free = False):
+def _generate_point_with_free_cs(point: Point, bias_towards_free : bool = False):
     """
     Frees all composition sets in point and sets NP to 1/n for all CS
         If bias_towards_free, then all free cs in point will have a weight of 2 when normalizing phase fractions
+
+    Parameters
+    ----------
+    point : Point
+        Point to generate new point from
+    bias_towards_free : bool
+        Whether to bias composition towards free phases of previous point
+
+    Returns
+    -------
+    new_point : Point
+        Point will all composition sets free
     """
     phase_sum = 0
     for cs in point.stable_composition_sets:
@@ -63,8 +153,6 @@ def _generate_point_with_free_cs(point: Point, bias_towards_free = False):
             update_cs_phase_frac(cs, 1/phase_sum)
         cs.fixed = False
 
-        # cs.fixed = False
-        # update_cs_phase_frac(cs, 1/len(new_point._free_composition_sets))
     for key in new_point.global_conditions:
         new_point.global_conditions[key] = new_point.get_property(key)
     return new_point