c12d60ba443c6e54e284d363b124eda4cbd0fdd6
Brandon Bocklund <bocklund1@llnl.gov>
2024-08-05 16:42:23 -0700

    text update

--- pycalphad/variables.py
+++ pycalphad/variables.py
@@ -247,7 +247,7 @@ class SiteFraction(StateVariable):
         state_variables = compsets[0].phase_record.state_variables
         result = np.atleast_1d(np.full(self.shape, fill_value=np.nan))
         for _, compset in self.filtered(compsets):
-            # we'll only hit this code path if this phase is in the stable set
+            # we'll only hit this code path if this phase is present in the list of compsets
             if np.all(np.isnan(result)):
                 result[0] = 0.0
             site_fractions = compset.phase_record.variables
