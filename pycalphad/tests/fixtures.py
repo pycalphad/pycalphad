436d3b1a9f1f7a9fce39623fe2400e4eddb00f80
Brandon Bocklund <bocklund1@llnl.gov>
2024-08-05 09:14:06 -0700

    WIP: convergence failure checks

--- pycalphad/tests/fixtures.py
+++ pycalphad/tests/fixtures.py
@@ -1,7 +1,9 @@
 from importlib.resources import files
 import pytest
+import numpy as np
 import pycalphad.tests.databases
 from pycalphad.io.database import Database
+from pycalphad.core.solver import Solver, SolverResult
 
 @pytest.fixture(scope="session")
 def load_database(request):
@@ -26,3 +28,12 @@ def select_database(path):
     ```
     """
     return pytest.mark.parametrize("load_database", [path], indirect=True)
+
+
+class ConvergenceFailureSolver(Solver):
+    """Solver that is guaranteed to produce a convergence failure for any composition sets or conditions passed"""
+    def solve(self, composition_sets, conditions):
+        spec = self.get_system_spec(composition_sets, conditions)
+        self._fix_state_variables_in_compsets(composition_sets, conditions)
+        state = spec.get_new_state(composition_sets)
+        return SolverResult(converged=False, x=composition_sets[0].dof, chemical_potentials=np.full_like(state.chemical_potentials, np.nan))
--- pycalphad/tests/test_workspace.py
+++ pycalphad/tests/test_workspace.py
@@ -5,7 +5,7 @@ from pycalphad.core.conditions import ConditionError
 from pycalphad.core.composition_set import CompositionSet
 from pycalphad.property_framework import as_property, ComputableProperty, T0, IsolatedPhase, DormantPhase
 from pycalphad.property_framework.units import Q_
-from pycalphad.tests.fixtures import load_database, select_database
+from pycalphad.tests.fixtures import load_database, select_database, ConvergenceFailureSolver
 import pytest
 from collections import Counter
 
@@ -316,3 +316,15 @@ def test_issue_503_pure_vacancy_charge_balance():
     wks = Workspace(TDB, ['O', 'ZR', 'VA'], ['SPINEL', 'GAS'], {v.P: 1e5, v.X('O'): 1, v.T: 1000})
     assert np.isnan(wks.get('NP(SPINEL)'))
     np.testing.assert_almost_equal(wks.get('NP(GAS)'), 1.0)
+
+
+@select_database("cumg.tdb")
+def test_workspace_convergence_failures(load_database):
+    dbf = load_database()
+    components = ["CU", "MG", "VA"]
+    phases = ["LIQUID", "FCC_A1"]
+    wks = Workspace(dbf, components, phases, {v.N:1, v.P:1e5, v.T:300, v.X("MG"): 0.5}, solver=ConvergenceFailureSolver())
+    assert len(list(wks.enumerate_composition_sets())[1]) == 0
+    assert np.isnan(wks.get("GM"))
+    print(wks.get(IsolatedPhase("LIQUID", wks=wks)("GM")))  # TODO: what should this do in convergence failure?
+    assert np.isnan(wks.get("MU(CU)"))
