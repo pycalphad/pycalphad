These print statements were made to better understand what is causing the now more probable heat capacity calculation bug with MQMQA

pycalphad/core/workspace.py
@@ -498,12 +498,10 @@ class Workspace:
         prop_MU_values = self.eq.MU
         str_conds_keys = [str(k) for k in self.eq.coords.keys() if k not in ('vertex', 'component', 'internal_dof')]
         conds_keys = [None] * len(str_conds_keys)
-        print('what are the args',args)
         for k in self.conditions.keys():
             cond_idx = str_conds_keys.index(str(k))
             conds_keys[cond_idx] = k
         local_index = 0
-
         for index, composition_sets in self.enumerate_composition_sets():
             cur_conds = OrderedDict(zip(conds_keys,
                                         [np.asarray(self.eq.coords[b][a], dtype=np.float64)
@@ -511,6 +509,7 @@ class Workspace:
             chemical_potentials = prop_MU_values[index]
 
             for arg in args:
+                print('THis is arg in args',arg)
                 prop_implementation_units, prop_display_units = arg_units[arg]
                 context = unit_conversion_context(composition_sets, arg)
                 if results.get(arg, None) is None:

pycalphad/property_framework/computed_property.py
@@ -63,6 +63,7 @@ class ModelComputedProperty(object):
             return None
 
     def compute_property(self, compsets: List[CompositionSet], cur_conds: Dict[str, float], chemical_potentials: npt.ArrayLike) -> npt.ArrayLike:
+        print('Is this the compute property in get?')
         if len(compsets) == 0:
             return np.nan
         if self.phase_name is None:
@@ -109,7 +110,7 @@ class ModelComputedProperty(object):
                 jansson_derivative += deltas.delta_phase_amounts[idx] * func_value
                 jansson_derivative += compset.NP * np.dot(deltas.delta_statevars, grad_value[:len(state_variables)])
                 jansson_derivative += compset.NP * np.dot(delta_sitefracs, grad_value[len(state_variables):])
-                print('Made it to this specific statement')
+                print('Made it to this specific statement',deltas.delta_phase_amounts[idx] * func_value)
             else:
                 jansson_derivative += np.dot(deltas.delta_statevars, grad_value[:len(state_variables)])
                 jansson_derivative += np.dot(delta_sitefracs, grad_value[len(state_variables):])
@@ -300,6 +301,7 @@ class JanssonDerivative:
         state.chemical_potentials[:] = chemical_potentials
         state.recompute(spec)
         deltas = self.denominator.jansson_deltas(spec, state)
+        print('Triple check how these spec and state properties are calculated',state)
         return self.numerator.jansson_derivative(compsets, cur_conds, chemical_potentials, deltas)
 
     def __str__(self):