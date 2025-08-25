2068bfd89ea4379878aab603ed468b0789e89378
bocklund <brandonbocklund@gmail.com>
2025-08-24 17:50:37 -0700

    Drop support for Python 3.10 following [NEP-29](https://numpy.org/neps/nep-0029-deprecation_policy.html)
    - NumPy build system requirement no longer needs a python_version specification since the minimum supported Python version (now 3.10) supports NumPy >2.
    - Dropping support for 3.10 "convienently" fixes consistent CI test failures on Ubuntu Python 3.10. Not all the logs report specific error codes, but several give error code 137 that indiciates an out of memory error, which seems to occur for the test `test_mapping_strategy.py::test_isopleth_strategy[crtiv_ghosh.tdb]` (runners that don't give clear error codes also fail here):
        - https://github.com/pycalphad/pycalphad/actions/runs/16898091136/job/48775321121
        - https://github.com/pycalphad/pycalphad/actions/runs/16739767492/job/47385601973
        - https://github.com/pycalphad/pycalphad/actions/runs/16161121473/job/45678928726

--- setup.py
+++ setup.py
@@ -90,7 +90,6 @@ setup(
 
         # Supported Python versions
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
