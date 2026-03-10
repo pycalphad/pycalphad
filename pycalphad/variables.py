506d5df78d4564dc33a535be8e6ac5d43f3e9ea7
Brandon Bocklund <bocklund1@llnl.gov>
2026-03-10 10:00:49 -0700

    lru_cache on StateVariable.__new__ to attempt to mitigate memory leak

--- pycalphad/variables.py
+++ pycalphad/variables.py
@@ -5,6 +5,7 @@ Classes and constants for representing thermodynamic variables.
 
 from typing import Optional
 from symengine import Float, Symbol
+from pycalphad.core.cache import lru_cache
 from pycalphad.io.grammar import parse_chemical_formula
 from pycalphad.property_framework.types import JanssonDerivativeDeltas
 from pycalphad.core.minimizer import site_fraction_differential, state_variable_differential, \
@@ -275,6 +276,10 @@ class StateVariable(Symbol):
     implementation_units = ''
     display_units = ''
 
+    @lru_cache(maxsize=10000)
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls, *args, **kwargs)
+
     @property
     def display_name(self):
         return self.name
@@ -482,7 +487,7 @@ class PhaseFraction(StateVariable):
 
     def expand_wildcard(self, phase_names):
         return [self.__class__(phase_name) for phase_name in phase_names]
-    
+
     def __reduce__(self):
         return self.__class__, (self.phase_name,)
 
@@ -847,7 +852,7 @@ class ChemicalPotential(StateVariable):
         return JanssonDerivativeDeltas(delta_chemical_potentials=delta_chemical_potentials, delta_statevars=delta_statevars,
                                    delta_phase_amounts=delta_phase_amounts, delta_sitefracs=compsets_delta_sitefracs,
                                    delta_parameters=None)
-    
+
     def __reduce__(self):
         return self.__class__, (self.species,)
 
