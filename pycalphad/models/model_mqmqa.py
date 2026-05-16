a642b3f9b81f35ca57d5df4e11ad270c39864383
Brandon Bocklund <bocklund1@llnl.gov>
2026-05-16 09:51:47 -0700

    Refactor MQMQA model changes for reciprocal support

--- pycalphad/models/model_mqmqa.py
+++ pycalphad/models/model_mqmqa.py
@@ -689,16 +689,16 @@ class ModelMQMQA(Model):
                         mixing_term *= Y_im / Xi_ikl * (1 - self._Y_ik(A, X) / Xi_ikl)**(r_alpha - 1)
                     else:  # not in nu or gamma
                         mixing_term *= Y_im * (1 - Xi_ikl - Xi_ilk)**(r_alpha - 1)
+            elif A != B and X != Y:
+                # An option for reciprocal interaction parameters in the FactSage software
+                pos_m = [spec for spec in m if spec.charge>0][0]
+                neg_m = [spec for spec in m if spec.charge<0][0]
+                quadruplet_mole = X_ijkl(pos_m,pos_m,neg_m,neg_m)
+                mix_term_exponent = [num for num in exponents if num!=0][0]
+                mixing_term = quadruplet_mole ** mix_term_exponent
+
             else:
-                if m == None:
-                    mixing_term = S.One  # No mixing, this is a modification to the formation energy of this quadruplet
-                else:
-                    # An option for quadruplet interaction parameters in the FactSage software
-                    pos_m = [spec for spec in m if spec.charge>0][0]
-                    neg_m = [spec for spec in m if spec.charge<0][0]
-                    quadruplet_mole = X_ijkl(pos_m,pos_m,neg_m,neg_m)
-                    mix_term_exponent = [num for num in exponents if num!=0][0]
-                    mixing_term = quadruplet_mole ** mix_term_exponent
+                mixing_term = S.One  # No mixing, this is a modification to the formation energy of this quadruplet
             g = param["parameter"] * mixing_term
 
             # Poschmann Eq. 17
