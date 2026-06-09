5c789a7fdaab359b58d3311bbbcbb4c2c6268e47
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-06-09 14:31:11 -0300

    Added a task to update current example automatically in command-line using new script `update_current_example.sh`

---
+++ update_current_example.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -f .venv/bin/activate ]; then
+    # for Unix-like OS, e.g. Linux, macOS
+    source .venv/bin/activate
+else
+    # for Windows
+    source .venv/Scripts/activate
+fi
+
+echo -e "\nUpdating current example...\n"$1"\n"
+
+currentPythonWarnings=$PYTHONWARNINGS
+echo -e "\nTemporarily setting PYTHONWARNINGS to 'ignore' to suppress warnings during notebook execution.\n"
+export PYTHONWARNINGS="ignore"
+
+cd "$(dirname "$1")"
+pwd
+
+jupyter nbconvert --to notebook --execute "$1" --output "$1"
+
+echo -e "\nFinished updating all notebooks in the examples directory.\n"
+echo -e "Restoring original PYTHONWARNINGS value: '$currentPythonWarnings'\n"
+export PYTHONWARNINGS="$currentPythonWarnings"
