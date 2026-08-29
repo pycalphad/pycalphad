67676574c890ff8ee174dc9034ac62d789e962e4
bocklund <brandonbocklund@gmail.com>
2026-08-29 16:28:13 -0700

    revert fix

--- pycalphad/core/calculate.py
+++ pycalphad/core/calculate.py
@@ -85,7 +85,7 @@ def _sample_phase_constitution(model, sampler, fixed_grid, pdens, phase_local_co
     ALLOWED_CHARGE=1E-10
     vacancy_indices = []
     for sublattice in model.constituents:
-        subl_va_indices = [idx for idx, spec in enumerate(sorted(set(sublattice), key=str)) if spec.number_of_atoms == 0]
+        subl_va_indices = [idx for idx, spec in enumerate(sorted(set(sublattice), key=lambda s: s.escaped_name)) if spec.number_of_atoms == 0]
         vacancy_indices.append(subl_va_indices)
     if len(vacancy_indices) != len(model.constituents):
         vacancy_indices = None
@@ -103,7 +103,10 @@ def _sample_phase_constitution(model, sampler, fixed_grid, pdens, phase_local_co
             constant_site_ratios = False
     species_charge = []
     for sublattice in range(len(model.constituents)):
-        for species in sorted(model.constituents[sublattice], key=str):
+        # Points columns are ordered following model.site_fractions, which sorts by the
+        # string of the SiteFraction, that uses species escaped_name sorting.
+        # This may differ from species.name sorting, particularly when species are charged.
+        for species in sorted(model.constituents[sublattice], key=lambda s: s.escaped_name):
             species_charge.append(species.charge*site_ratios[sublattice])
     species_charge = np.array(species_charge)
     charge_constrained_space = constant_site_ratios and np.any(species_charge != 0)
